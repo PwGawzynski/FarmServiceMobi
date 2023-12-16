75d671e70ffe8d1106a6b1ee9d86b92e4812aa26
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-16 16:34:31 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { H3, Stack, TamaguiProvider } from 'tamagui';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { SafeAreaView } from 'react-native';
+// eslint-disable-next-line import/extensions
+import config from './tamagui.config';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider defaultTheme="dark" config={config}>
+      <Stack f={1} pl="$4" pr="$4">
+        <SafeAreaView style={{ flex: 1 }}>
+          <H3 style={{ textTransform: 'uppercase' }}>Notifications</H3>
+        </SafeAreaView>
+      </Stack>
+    </TamaguiProvider>
   );
 }
