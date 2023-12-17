c2895bacb520a99881222eda301b8c95beb09354
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-17 12:53:13 +0100

    added redux provider, auth navigation driver

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-import { H3, Stack, TamaguiProvider } from 'tamagui';
+import { TamaguiProvider } from 'tamagui';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
 // eslint-disable-next-line import/extensions
 import config from './tamagui.config';
+import AuthDriver from './components/navigators/AuthDriver';
+import store from './src/redux/app/Store';
 
 export default function App() {
   const [loaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterItalic: require('@tamagui/font-inter/otf/Inter-MediumItalic.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    InterBoldItalic: require('@tamagui/font-inter/otf/Inter-BoldItalic.otf'),
+    InterSemiBold: require('@tamagui/font-inter/otf/Inter-SemiBold.otf'),
+    InterSemiBoldItalic: require('@tamagui/font-inter/otf/Inter-SemiBoldItalic.otf'),
+    InterExtraBold: require('@tamagui/font-inter/otf/Inter-ExtraBold.otf'),
+    InterExtraBoldItalic: require('@tamagui/font-inter/otf/Inter-ExtraBoldItalic.otf'),
   });
 
   useEffect(() => {
@@ -22,12 +31,12 @@ export default function App() {
   }
 
   return (
-    <TamaguiProvider defaultTheme="dark" config={config}>
-      <Stack f={1} pl="$4" pr="$4">
-        <SafeAreaView style={{ flex: 1 }}>
-          <H3 style={{ textTransform: 'uppercase' }}>Notifications</H3>
-        </SafeAreaView>
-      </Stack>
-    </TamaguiProvider>
+    <Provider store={store}>
+      <TamaguiProvider defaultTheme="dark" config={config}>
+        <NavigationContainer>
+          <AuthDriver />
+        </NavigationContainer>
+      </TamaguiProvider>
+    </Provider>
   );
 }
