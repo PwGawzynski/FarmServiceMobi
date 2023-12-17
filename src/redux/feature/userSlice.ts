c34eff43e0cf2a0c0579ba89483c566916a2f631
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-17 12:56:49 +0100

    added theme to user slice

--- src/redux/feature/userSlice.ts
+++ src/redux/feature/userSlice.ts
@@ -1,9 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+type UserContextI = {
+  theme: number;
+};
+
+export type UserSliceI = {
+  user: UserContextI;
+};
+
 const UserSlice = createSlice({
   name: 'user',
-  initialState: {},
+  initialState: {
+    theme: 1,
+  } as UserContextI,
   reducers: {},
 });
+
+export const selectTheme = (state: UserSliceI) => state.user.theme;
 
 export default UserSlice.reducer;
