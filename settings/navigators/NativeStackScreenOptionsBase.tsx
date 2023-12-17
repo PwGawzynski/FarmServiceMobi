ff0bd6a5ab0ecc67f5027e9a6c92728e76004f5c
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-17 12:55:09 +0100

    created setting file for native stack navigator

---
+++ settings/navigators/NativeStackScreenOptionsBase.tsx
@@ -0,0 +1,7 @@
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+
+export const NativeStackScreenOptionsBase = {
+  gestureDirection: 'vertical',
+  headerShown: false,
+  gestureEnabled: true,
+} as NativeStackNavigationOptions;
