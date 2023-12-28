46c7422459cb7ae810eeaed805cbc67fe4609e64
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-28 14:37:54 +0100

    template conponent tmplate

--- components/atoms/OrDivider.tsx
+++ components/atoms/OrDivider.tsx
@@ -1,13 +1,5 @@
-import { Separator, Text, XStack } from 'tamagui';
+import { View } from 'react-native';
 
 export function OrDivider() {
-  return (
-    <XStack mt="$4" mb="$4" alignItems="center">
-      <Separator />
-      <Text ml="$2" mr="$2" color="$background">
-        OR
-      </Text>
-      <Separator />
-    </XStack>
-  );
+  return <View />;
 }
