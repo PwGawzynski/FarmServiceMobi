eb3d4dccb9b0e497441f0e29f2af69d911408267
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-28 14:35:53 +0100

    added reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // optional, only if you ever use process.env
-      "transform-inline-environment-variables",
-      // NOTE: this is optional, you don't *need* the compiler
-      [
-        "@tamagui/babel-plugin",
-        {
-          components: ["tamagui"],
-          config: "./tamagui.config.ts",
-          logTimings: true,
-        },
-      ],
-      // NOTE: this is only necessary if you are using reanimated for animations
-      "react-native-reanimated/plugin",
+      "nativewind/babel",
+      'react-native-reanimated/plugin'
     ],
   };
 };
