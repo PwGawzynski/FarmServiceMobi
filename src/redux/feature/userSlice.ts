6755356ccf7c008c2bfae8c05cb86531798b9b1d
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-28 14:41:34 +0100

    changed default theme

--- src/redux/feature/userSlice.ts
+++ src/redux/feature/userSlice.ts
@@ -37,7 +37,7 @@ const UserSlice = createSlice({
       email: undefined,
       password: undefined,
       isActivated: undefined,
-      theme: Theme.dark,
+      theme: Theme.light,
       activationCode: undefined,
     },
   } as UserContextI,
