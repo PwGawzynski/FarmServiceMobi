70daa56a2c0137344e3df84070981c28078db532
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-28 14:37:15 +0100

    base for logo component

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -1,22 +1,5 @@
-import { useTheme, XStack, YStack } from 'tamagui';
-import Location from '../../assets/location.svg';
-import { Text } from './Text';
+import { View } from 'react-native';
 
 export function Logo() {
-  const { background } = useTheme();
-
-  return (
-    <XStack mt="$8" h="$6" justifyContent="center" alignItems="center">
-      <XStack>
-        <YStack w="20%">
-          <Location width="100%" height="100%" fill={background?.val} />
-        </YStack>
-        <YStack justifyContent="center">
-          <Text fontStyle="italic" fontSize={40}>
-            FarmService
-          </Text>
-        </YStack>
-      </XStack>
-    </XStack>
-  );
+  return <View />;
 }
