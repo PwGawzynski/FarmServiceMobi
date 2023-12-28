2f3837fc1f40ec8f7dcecf6a819eb4a4d6dffd06
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-28 14:40:50 +0100

    created hook for getting stored theme

--- helepers/ThemeHelpers.tsx
+++ helepers/ThemeHelpers.tsx
@@ -1,18 +1,16 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Theme } from '../FarmServiceApiTypes/Account/Constants';
 
-export async function getThemeFromStorage() {
+export async function getThemeFromStorage(): Promise<Theme | undefined> {
   const storedTheme = await AsyncStorage.getItem('theme');
-  if (storedTheme) return storedTheme;
+  console.log(storedTheme, 'FFFF');
+  if (storedTheme) return JSON.parse(storedTheme) as Theme;
   return undefined;
 }
 
 export async function setThemeToStorage(theme: Theme) {
   try {
-    await AsyncStorage.setItem(
-      'theme',
-      theme === Theme.dark ? 'dark' : 'light',
-    );
+    await AsyncStorage.setItem('theme', theme.toString());
     return true;
   } catch (e) {
     return false;
