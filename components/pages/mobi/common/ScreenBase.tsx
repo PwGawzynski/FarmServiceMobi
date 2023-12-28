7a195d03e9bd090b4cbfe72e3abcad945bf80ff9
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-28 14:38:30 +0100

    created base screen card

--- components/pages/mobi/common/ScreenBase.tsx
+++ components/pages/mobi/common/ScreenBase.tsx
@@ -1,28 +1,35 @@
-import { H2, Stack, XStack } from 'tamagui';
-import React from 'react';
-import { FlexAlignType, SafeAreaView } from 'react-native';
-import { ScreenCard } from '../../../atoms/ScreenCard';
+import React, { useEffect } from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { isNull } from '@notifee/react-native/dist/utils';
+import { UseStoredTheme } from '../../../../hooks/UseStoredTheme';
 
 export type Props = {
-  name: string;
+  name?: string;
   children?: React.ReactNode;
-  filed?: boolean;
-  align?: FlexAlignType;
 };
 
-export function ScreenBase({ name, children, filed, align }: Props) {
+export function ScreenBase({ name, children }: Props) {
+  const { theme } = UseStoredTheme();
+  const { colorScheme, setColorScheme } = useColorScheme();
+  useEffect(() => {
+    console.log(theme, 'APP_THEME');
+    if (!isNull(theme)) {
+      setColorScheme(theme === 0 ? 'light' : 'dark');
+    }
+  }, [theme]);
   return (
-    <ScreenCard filed={filed}>
-      <SafeAreaView style={{ flex: 1, width: '100%' }}>
-        <Stack w="100%" alignItems={align ?? 'center'} f={1}>
-          <XStack width="100%">
-            <H2 textAlign="left" textTransform="uppercase">
-              {name}
-            </H2>
-          </XStack>
-          {children}
-        </Stack>
-      </SafeAreaView>
-    </ScreenCard>
+    <SafeAreaView
+      className={`flex-1 ${colorScheme === 'dark' ? 'bg-dark' : 'bg-white'}`}
+    >
+      <View className="flex-1 ml-4 mr-4">
+        <View className="w-full">
+          <Text className="text-2xl uppercase font-bold text-dark dark:text-green">
+            {name}
+          </Text>
+        </View>
+        {children}
+      </View>
+    </SafeAreaView>
   );
 }
