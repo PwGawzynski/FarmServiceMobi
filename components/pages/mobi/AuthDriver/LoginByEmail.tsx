71fee2b7acfed9ea5296ed621ff2ecb293409659
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-30 19:36:37 +0100

    created file for login by email screen

---
+++ components/pages/mobi/AuthDriver/LoginByEmail.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { ScreenBase } from '../common/ScreenBase';
+
+export function LoginByEmail() {
+  return (
+    <ScreenBase>
+      <Text>LoginByEmail</Text>
+    </ScreenBase>
+  );
+}
