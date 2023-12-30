3820e486203dfcf25cea294c46fcd14d70c3fa69
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-30 19:32:56 +0100

    Created styled app button

---
+++ components/atoms/AppButton.tsx
@@ -0,0 +1,24 @@
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { getFontScaledSize } from '../../helepers/FontSize';
+
+export type Props = {
+  title: string;
+} & TouchableOpacityProps;
+
+export function AppButton(props: Props) {
+  return (
+    <TouchableOpacity
+      className="flex-1 max-h-12 bg-dark dark:bg-green items-center justify-center rounded-full"
+      {...props}
+    >
+      <Text
+        numberOfLines={1}
+        adjustsFontSizeToFit
+        style={{ fontSize: getFontScaledSize(18) }}
+        className="text-center align-middle text-green font-bold uppercase dark:text-dark pl-4 pr-4"
+      >
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
