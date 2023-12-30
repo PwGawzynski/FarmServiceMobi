377816d94c7eb84b2fcde37209223983ab518006
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-30 19:34:18 +0100

    changed name of screen, added new screens landingByEmail and choose register type

--- components/navigators/AuthDriver.tsx
+++ components/navigators/AuthDriver.tsx
@@ -3,6 +3,8 @@ import { AuthDriverParamList } from '../../types/self/navigation/paramLists/Auth
 import Landing from '../pages/mobi/AuthDriver/Landing';
 import { NativeStackScreenOptionsBase } from '../../settings/navigators/NativeStackScreenOptionsBase';
 import { ChooseLoginType } from '../pages/mobi/AuthDriver/ChooseLoginType';
+import { LoginByEmail } from '../pages/mobi/AuthDriver/LoginByEmail';
+import { ChooseRegisterType } from '../pages/mobi/AuthDriver/ChooseRegisterType';
 
 const Stack = createNativeStackNavigator<AuthDriverParamList>();
 
@@ -24,8 +26,26 @@ export default function AuthDriver() {
           gestureEnabled: false,
           animation: 'fade',
         }}
-        name="login"
+        name="chooseLoginType"
         component={ChooseLoginType}
+      />
+      <Stack.Screen
+        options={{
+          ...NativeStackScreenOptionsBase,
+          animation: 'fade',
+          gestureDirection: 'horizontal',
+        }}
+        name="loginByEmail"
+        component={LoginByEmail}
+      />
+      <Stack.Screen
+        options={{
+          ...NativeStackScreenOptionsBase,
+          animation: 'fade',
+          gestureDirection: 'horizontal',
+        }}
+        name="chooseRegisterType"
+        component={ChooseRegisterType}
       />
     </Stack.Navigator>
   );
