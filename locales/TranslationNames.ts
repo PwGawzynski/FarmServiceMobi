485d8a6166df0c1a2214fe048eedf4f530fb964a
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-30 19:37:38 +0100

    created object to map translation json to object

---
+++ locales/TranslationNames.ts
@@ -0,0 +1,23 @@
+export const TranslationNames = {
+  screens: {
+    authDriver: {
+      chooseLoginType: {
+        header: 'screens.AuthDriver.chooseLoginType.header',
+        email: 'screens.AuthDriver.chooseLoginType.email',
+        google: 'screens.AuthDriver.chooseLoginType.google',
+        registerButton: 'screens.AuthDriver.chooseLoginType.registerButton',
+        registerText: 'screens.AuthDriver.chooseLoginType.registerText',
+        instruction: 'screens.AuthDriver.chooseLoginType.instruction',
+      },
+      landing: {
+        connecting: 'screens.AuthDriver.landing.connecting',
+      },
+      chooseRegisterType: {
+        header: 'screens.AuthDriver.chooseRegisterType.header',
+        email: 'screens.AuthDriver.chooseRegisterType.email',
+        google: 'screens.AuthDriver.chooseRegisterType.google',
+        instruction: 'screens.AuthDriver.chooseRegisterType.instruction',
+      },
+    },
+  },
+};
