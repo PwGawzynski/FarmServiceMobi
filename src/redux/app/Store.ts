1cccd89d151a668a278ee4afa00f3e7fdbce20ef
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-11 22:58:15 +0100

    chaged export type

--- src/redux/app/Store.ts
+++ src/redux/app/Store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import UserReducer from '../feature/userSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     user: UserReducer,
   },
 });
+export default store;
+export type AppDispatch = typeof store.dispatch;
