ca2359ece0c38b86417736ab24c75a213e64b892
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-11 22:53:16 +0100

    create styling helper file

--- settings/styles/colors.ts
+++ settings/styles/colors.ts
@@ -1,7 +1,9 @@
 export enum Colors {
   DARK = '#081e3f',
-  DARK_GREEN = '#083d56',
-  LIGHT_GREEN = '#0e5f76',
+  DARK_BLUE = '#083d56',
+  LIGHT_BLUE = '#0e5f76',
   GREEN = '#d7df71',
   WHITE = '#ffffff',
+  ERROR_RED = '#fd6363',
+  DARK_GRAY = '#939292',
 }
