57d96e91e3037d1b01c93492cb53029ebc2c656a
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-11 22:58:27 +0100

    added more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
     extend: {},
     colors:{
       "dark": '#081e3f',
-      "dark-green": '#083d56',
-      "light-green": '#0e5f76',
+      "dark-blue": '#083d56',
+      "light-blue": '#0e5f76',
       "green": '#d7df71',
+      "dark-gray": '#939292',
+      "error-red": "#fd6363",
+      "activity-dot": "#ff4400"
     }
   },
   plugins: [],
