34d4b6f142bc352a514c0eaeed5aeee1b01dd3c9
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-11 22:58:03 +0100

    changed logic -> added tailwindInt base on storage

--- components/pages/mobi/common/ScreenBase.tsx
+++ components/pages/mobi/common/ScreenBase.tsx
@@ -1,33 +1,37 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { useColorScheme } from 'nativewind';
-import { isNull } from '@notifee/react-native/dist/utils';
-import { UseStoredTheme } from '../../../../hooks/UseStoredTheme';
+import { useSelector } from 'react-redux';
+import { Theme } from '../../../../FarmServiceApiTypes/Account/Constants';
+import { selectTheme } from '../../../../src/redux/feature/userSlice';
 
 export type Props = {
   name?: string;
   children?: React.ReactNode;
+  activityDot?: boolean;
 };
 
-export function ScreenBase({ name, children }: Props) {
-  const { theme } = UseStoredTheme();
-  const { colorScheme, setColorScheme } = useColorScheme();
+export function ScreenBase({ name, children, activityDot }: Props) {
+  const theme = useSelector(selectTheme);
+  const { setColorScheme } = useColorScheme();
   useEffect(() => {
-    console.log(theme, 'APP_THEME');
-    if (!isNull(theme)) {
-      setColorScheme(theme === 0 ? 'light' : 'dark');
-    }
+    setColorScheme(theme === Theme.light ? 'light' : 'dark');
   }, [theme]);
   return (
-    <SafeAreaView
-      className={`flex-1 ${colorScheme === 'dark' ? 'bg-dark' : 'bg-white'}`}
-    >
+    <SafeAreaView className="flex-1 bg-white dark:bg-dark">
       <View className="flex-1 ml-4 mr-4">
-        <View className="w-full">
-          <Text className="text-2xl uppercase font-bold text-dark dark:text-green">
-            {name}
-          </Text>
+        <View className="w-full h-2  items-end">
+          {activityDot && (
+            <View className="h-2 w-2 rounded-full bg-activity-dot" />
+          )}
         </View>
+        {name && (
+          <View className="w-full">
+            <Text className="text-2xl uppercase font-bold text-dark dark:text-green">
+              {name}
+            </Text>
+          </View>
+        )}
         {children}
       </View>
     </SafeAreaView>
