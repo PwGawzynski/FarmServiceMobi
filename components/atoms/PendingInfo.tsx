93953041d48b5919473545ef1402d3e42e90c3e5
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-13 17:38:23 +0100

    created pending info component

---
+++ components/atoms/PendingInfo.tsx
@@ -0,0 +1,36 @@
+import { ActivityIndicator, Text } from 'react-native';
+import { useEffect } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { Colors } from '../../settings/styles/colors';
+
+export type Props = {
+  infoText?: string;
+  isVisible: boolean;
+};
+
+export function PendingInfo({ infoText, isVisible }: Props) {
+  const opacity = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => ({ opacity: opacity.value }));
+
+  useEffect(() => {
+    if (isVisible) opacity.value = withTiming(1, { duration: 200 });
+    return () => {
+      opacity.value = withTiming(0, { duration: 100 });
+    };
+  }, [isVisible]);
+
+  if (!isVisible) return null;
+  return (
+    <Animated.View
+      style={[animatedStyle]}
+      className="flex-1 flex-row items-center justify-center"
+    >
+      <ActivityIndicator color={Colors.GREEN} />
+      <Text className="ml-2 text-dark-gray">{infoText}</Text>
+    </Animated.View>
+  );
+}
