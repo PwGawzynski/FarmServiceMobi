2b7a859013befc139eb9e041a34aab94fef749fa
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-15 22:26:18 +0100

    created navigator

---
+++ components/navigators/owner/activities/ActivitiesDriver.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ActivityDriverParamList } from '../../../../types/self/navigation/paramLists/ActivityDriverParamList';
+import { NativeStackScreenOptionsBase } from '../../../../settings/navigators/NativeStackScreenOptionsBase';
+import { ActivitiesDesktopDriver } from './ActivitiesDesktopDriver';
+
+const Stack = createNativeStackNavigator<ActivityDriverParamList>();
+
+export function ActivityDriver() {
+  return (
+    <Stack.Navigator
+      screenOptions={NativeStackScreenOptionsBase}
+      initialRouteName="activityDesktopRoot"
+    >
+      <Stack.Screen
+        name="activityDesktopRoot"
+        component={ActivitiesDesktopDriver}
+      />
+    </Stack.Navigator>
+  );
+}
