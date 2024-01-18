50b2a2898dd9c5f04acacb8f4783d0d18ad6b805
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-18 11:49:10 +0100

    set up navigator header to work with theme

--- components/navigators/owner/clients/ClientsDesktopDriver.tsx
+++ components/navigators/owner/clients/ClientsDesktopDriver.tsx
@@ -1,14 +1,28 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useSelector } from 'react-redux';
 import { ClientsDesktopDriverParamList } from '../../../../types/self/navigation/paramLists/clients/ClientsDesktopDriverParamList';
 import { ClientsDesktop } from '../../../pages/mobi/ClientsDesktopDriver/ClientsDesktop';
-import { MaterialTopTabScreenOptions } from '../../../../settings/navigators/MaterialTopTabScreenOptions';
+import {
+  MaterialBaseInitialLayout,
+  MaterialTopTabScreenOptions,
+} from '../../../../settings/navigators/MaterialTopTabScreenOptions';
+import { Theme } from '../../../../FarmServiceApiTypes/Account/Constants';
+import { Colors } from '../../../../settings/styles/colors';
+import { selectTheme } from '../../../../src/redux/feature/userSlice';
 
 const TopTab = createMaterialTopTabNavigator<ClientsDesktopDriverParamList>();
 
 export function ClientsDesktopDriver() {
+  const theme = useSelector(selectTheme);
+
   return (
     <TopTab.Navigator
       initialRouteName="clientsDesktop"
+      style={{
+        backgroundColor: theme === Theme.dark ? Colors.DARK : Colors.WHITE,
+        paddingBottom: '5%',
+      }}
+      initialLayout={MaterialBaseInitialLayout}
       screenOptions={MaterialTopTabScreenOptions}
     >
       <TopTab.Screen name="clientsDesktop" component={ClientsDesktop} />
