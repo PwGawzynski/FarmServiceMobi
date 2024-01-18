08e3110220b189896cd886d18c1fde80e1bad00a
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-18 11:46:17 +0100

    added styles to style save area view top header

--- components/navigators/owner/activities/ActivitiesDesktopDriver.tsx
+++ components/navigators/owner/activities/ActivitiesDesktopDriver.tsx
@@ -1,13 +1,27 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useSelector } from 'react-redux';
 import { ActivityDesktopRoot } from '../../../pages/mobi/ActivitiesDesktopTopTabNavigator/ActivityDesktopRoot';
-import { MaterialTopTabScreenOptions } from '../../../../settings/navigators/MaterialTopTabScreenOptions';
+import {
+  MaterialBaseInitialLayout,
+  MaterialTopTabScreenOptions,
+} from '../../../../settings/navigators/MaterialTopTabScreenOptions';
 import { ActivityDesktopDriverParamList } from '../../../../types/self/navigation/paramLists/activities/ActivityDesktopDriverParamList';
+import { selectTheme } from '../../../../src/redux/feature/userSlice';
+import { Theme } from '../../../../FarmServiceApiTypes/Account/Constants';
+import { Colors } from '../../../../settings/styles/colors';
 
 const Stack = createMaterialTopTabNavigator<ActivityDesktopDriverParamList>();
 
 export function ActivitiesDesktopDriver() {
+  const theme = useSelector(selectTheme);
+
   return (
     <Stack.Navigator
+      style={{
+        backgroundColor: theme === Theme.dark ? Colors.DARK : Colors.WHITE,
+        paddingBottom: '5%',
+      }}
+      initialLayout={MaterialBaseInitialLayout}
       screenOptions={MaterialTopTabScreenOptions}
       initialRouteName="lastActivities"
     >
