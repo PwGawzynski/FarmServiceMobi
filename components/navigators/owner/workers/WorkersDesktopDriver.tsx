6b0e00ce7b8de08c05b7a6baabd0f590e89ce790
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-19 12:18:47 +0100

    added handling native for application navigation gestures, by adding RNGH2 and listening at fling gesture

--- components/navigators/owner/workers/WorkersDesktopDriver.tsx
+++ components/navigators/owner/workers/WorkersDesktopDriver.tsx
@@ -1,6 +1,12 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { useSelector } from 'react-redux';
 import {
+  Directions,
+  Gesture,
+  GestureDetector,
+} from 'react-native-gesture-handler';
+import { Platform } from 'react-native';
+import {
   MaterialBaseInitialLayout,
   MaterialTopTabScreenOptions,
 } from '../../../../settings/navigators/MaterialTopTabScreenOptions';
@@ -9,23 +15,38 @@ import { Theme } from '../../../../FarmServiceApiTypes/Account/Constants';
 import { Colors } from '../../../../settings/styles/colors';
 import { WorkersDesktopDriverParamList } from '../../../../types/self/navigation/paramLists/workers/WorkersDesktopDriverParamList';
 import { WorkersDesktop } from '../../../pages/mobi/WorkersDesktopDriver/WorkersDesktop';
+import { WorkersDriverScreenProps } from '../../../../types/self/navigation/props/workers/WorkersDriverProps';
 
 const TopTab = createMaterialTopTabNavigator<WorkersDesktopDriverParamList>();
 
-export function WorkersDesktopDriver() {
+export function WorkersDesktopDriver({
+  navigation,
+}: WorkersDriverScreenProps<
+  'workersDesktopRoot',
+  'workersDriver',
+  'ownerRootDriver'
+>) {
   const theme = useSelector(selectTheme);
-
+  const fling = Gesture.Fling();
+  fling
+    .direction(Directions.DOWN)
+    .enabled(Platform.OS === 'android')
+    .onEnd(() => {
+      navigation.goBack();
+    });
   return (
-    <TopTab.Navigator
-      style={{
-        backgroundColor: theme === Theme.dark ? Colors.DARK : Colors.WHITE,
-        paddingBottom: '5%',
-      }}
-      initialLayout={MaterialBaseInitialLayout}
-      screenOptions={MaterialTopTabScreenOptions}
-      initialRouteName="workersDesktop"
-    >
-      <TopTab.Screen name="workersDesktop" component={WorkersDesktop} />
-    </TopTab.Navigator>
+    <GestureDetector gesture={fling}>
+      <TopTab.Navigator
+        style={{
+          backgroundColor: theme === Theme.dark ? Colors.DARK : Colors.WHITE,
+          paddingBottom: '5%',
+        }}
+        initialLayout={MaterialBaseInitialLayout}
+        screenOptions={MaterialTopTabScreenOptions}
+        initialRouteName="workersDesktop"
+      >
+        <TopTab.Screen name="workersDesktop" component={WorkersDesktop} />
+      </TopTab.Navigator>
+    </GestureDetector>
   );
 }
