c3e08d3ffb0bdf6885094eff3033e5bd237b2e28
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-04 11:12:39 +0100

    added tamagui conf file

---
+++ tamagui_conf/tamagui.config.ts
@@ -0,0 +1,32 @@
+import { createTamagui } from 'tamagui';
+import { shorthands } from '@tamagui/shorthands';
+import { animations } from './animations';
+import { fonts } from './fonts';
+import { media } from './media';
+import { themes } from './themes';
+import { tokens } from './tokens';
+
+const config = createTamagui({
+  defaultFont: 'body',
+  animations,
+  shouldAddPrefersColorThemes: true,
+  themeClassNameOnRoot: true,
+  shorthands,
+  fonts,
+  themes,
+  tokens,
+  media,
+});
+
+type AppConfig = typeof config;
+
+declare module 'tamagui' {
+  // overrides TamaguiCustomConfig so that custom types
+  // work everywhere `tamagui` is imported
+  interface TamaguiCustomConfig extends AppConfig {}
+  interface TypeOverride {
+    groupNames(): 'a' | 'b' | 'c';
+  }
+}
+
+export default config;
