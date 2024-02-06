392cee5052a6e7ea571f20debbedd8bfc56e2c3c
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-06 08:11:40 +0100

    added client service method to serve getting clients

--- api/clients/Client.tsx
+++ api/clients/Client.tsx
@@ -1,18 +1,26 @@
 import { t } from 'i18next';
 import { Api } from '../Api';
 import { apiHandler } from '../services/User';
-import { ResponseObject } from '../../FarmServiceApiTypes/Respnse/responseGeneric';
 import { TranslationNames } from '../../locales/TranslationNames';
 import { ClientResponseBase } from '../../FarmServiceApiTypes/Clients/Responses';
 import { CreateClientReqI } from '../../FarmServiceApiTypes/Clients/Requests';
 
-export async function createCompany(data: CreateClientReqI) {
+export async function createClient(data: CreateClientReqI) {
   const UNAUTHORIZED_MSG = t(TranslationNames.serviceDefaults.unauthorised);
   const DEFAULT_MSG = t(TranslationNames.serviceDefaults.default);
-  return apiHandler<CreateClientReqI>(
+  return apiHandler<CreateClientReqI, ClientResponseBase | undefined>(
     UNAUTHORIZED_MSG,
     DEFAULT_MSG,
     Api.createClient,
     data,
-  ) as unknown as ResponseObject<ClientResponseBase>;
+  ) as unknown as ClientResponseBase | undefined;
+}
+export async function getClients() {
+  const UNAUTHORIZED_MSG = t(TranslationNames.serviceDefaults.unauthorised);
+  const DEFAULT_MSG = t(TranslationNames.serviceDefaults.default);
+  return apiHandler<ClientResponseBase>(
+    UNAUTHORIZED_MSG,
+    DEFAULT_MSG,
+    Api.getClients,
+  ) as unknown as Array<ClientResponseBase> | undefined;
 }
