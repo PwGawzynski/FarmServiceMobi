bd795ba8da543063eb75868786a58e46937ebce1
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-06 08:15:29 +0100

    A list of clients has been created

---
+++ components/organisms/ClientsList.tsx
@@ -0,0 +1,22 @@
+import { ListRenderItemInfo } from '@shopify/flash-list';
+import { useCallback } from 'react';
+import { VerticalList } from '../molecules/VerticalList';
+import { ClientResponseBase } from '../../FarmServiceApiTypes/Clients/Responses';
+import { ClientListItem } from '../molecules/ClientListItem';
+
+export type Props = {
+  data?: Array<ClientResponseBase>;
+};
+
+export function ClientList({ data }: Props) {
+  const renderItem = useCallback(
+    ({ item }: ListRenderItemInfo<ClientResponseBase>) => (
+      <ClientListItem user={item.user} />
+    ),
+    [],
+  );
+
+  return (
+    <VerticalList estimatedSize={150} renderItem={renderItem} data={data} />
+  );
+}
