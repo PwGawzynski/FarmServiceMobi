31a97cd22c2ea65b19a4f8761898524aa7c32a2f
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-07 08:08:41 +0100

    added - params client's details screen

--- types/self/navigation/paramLists/clients/ClientsDriverParamList.tsx
+++ types/self/navigation/paramLists/clients/ClientsDriverParamList.tsx
@@ -1,6 +1,10 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
 import { ClientsDesktopDriverParamList } from './ClientsDesktopDriverParamList';
+import { ClientResponseBase } from '../../../../../FarmServiceApiTypes/Clients/Responses';
 
 export type ClientsDriverParamList = {
   clientsDesktopRoot: NavigatorScreenParams<ClientsDesktopDriverParamList>;
+  clientDetails: {
+    client: ClientResponseBase;
+  };
 };
