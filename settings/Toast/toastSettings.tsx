6a3ba73ace5d4630f34f204c810289685c560627
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-07 08:20:04 +0100

    fix env val loading

--- settings/Toast/toastSettings.tsx
+++ settings/Toast/toastSettings.tsx
@@ -1,4 +1,10 @@
-export const TOAST_DURATION =
-  (Number.isNaN(Number(process.env.TOAST_DURATION)) &&
-    Number(process.env.TOAST_DURATION)) ||
-  3000;
+if (Number.isNaN(Number(process.env.EXPO_PUBLIC_TOAST_DURATION))) {
+  console.error(
+    'Error: EXPO_PUBLIC_TOAST_DURATION is not a number is: ',
+    process.env.EXPO_PUBLIC_TOAST_DURATION,
+  );
+}
+
+export const TOAST_DURATION = Number(
+  process.env.EXPO_PUBLIC_TOAST_DURATIONTOAST_DURATION,
+);
