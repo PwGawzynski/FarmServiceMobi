a771840cd3904f39b2d5e5fb98c13b8cc9497c2f
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-07 09:32:41 +0100

    created call button

--- components/atoms/CallButton.tsx
+++ components/atoms/CallButton.tsx
@@ -1,24 +1,38 @@
-// import * as Linking from 'expo-linking';
+import * as Linking from 'expo-linking';
 import { Button, useTheme } from 'tamagui';
+import { t } from 'i18next';
 import Phone from '../../assets/phone.svg';
+import { TranslationNames } from '../../locales/TranslationNames';
 
-type CallButtonProps = {
+export type CallButtonProps = {
   phoneNumber: string;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function CallButton({ phoneNumber }: CallButtonProps) {
-  // const onCallAction = () => Linking.openURL(`tel:${phoneNumber}`);
-  const { color9 } = useTheme();
-  console.log(color9);
+  const onCallAction = () => Linking.openURL(`tel:${phoneNumber}`);
+  const { color4 } = useTheme();
   return (
     <Button
-      onPress={() => {
-        //  onCallAction();
-      }}
+      f={1}
+      pressStyle={
+        {
+          color: color4.val,
+        } as never
+      }
+      onPress={onCallAction}
     >
-      <Phone color={color9.val} />
-      Call
+      <Button.Icon>
+        <Phone />
+      </Button.Icon>
+      <Button.Text
+        size="$4"
+        fontWeight="bold"
+        adjustsFontSizeToFit
+        numberOfLines={1}
+      >
+        {t(TranslationNames.components.button.call)}
+      </Button.Text>
     </Button>
   );
 }
