5206656e693f5d1983b7c34e71b6db4dad763c20
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-07 08:19:52 +0100

    fix env val loading

--- settings/theme/themeSettings.tsx
+++ settings/theme/themeSettings.tsx
@@ -1,6 +1,7 @@
-import { Theme } from '../../FarmServiceApiTypes/Account/Constants';
-
-export const DEFAULT_THEME =
-  (Number.isNaN(Number(process.env.DEFAULT_THEME)) &&
-    Number(process.env.DEFAULT_THEME)) ||
-  Theme.light;
+if (Number.isNaN(Number(process.env.EXPO_PUBLIC_DEFAULT_THEME))) {
+  console.error(
+    'Error: EXPO_PUBLIC_DEFAULT_THEME is not a number is: ',
+    process.env.EXPO_PUBLIC_DEFAULT_THEME,
+  );
+}
+export const DEFAULT_THEME = Number(process.env.EXPO_PUBLIC_DEFAULT_THEME);
