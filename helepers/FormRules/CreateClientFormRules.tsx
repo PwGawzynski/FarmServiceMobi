3dd45f0340a42b47c9d7403bf947d2fd7823e862
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-11 16:03:32 +0100

    some rules moved to common

--- helepers/FormRules/CreateClientFormRules.tsx
+++ helepers/FormRules/CreateClientFormRules.tsx
@@ -2,27 +2,12 @@ import { t } from 'i18next';
 import { FormRulesType } from './FormRulesType';
 import { CreateClientForm } from '../../components/pages/mobi/ClientsDesktopDriver/CreateClient';
 import { TranslationNames } from '../../locales/TranslationNames';
-import AddressConstants from '../../FarmServiceApiTypes/Address/Constants';
 import { creteMinLenMessage } from './FormRulesHelper';
-import { LoginUserConstants } from '../../FarmServiceApiTypes/User/Constants';
 import { UserPersonalDataConstants } from '../../FarmServiceApiTypes/UserPersonalData/Constants';
+import { AddressRules, CommonRules } from './CommonRules';
 
 export const rules: FormRulesType<CreateClientForm> = {
-  email: {
-    pattern: {
-      value: /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
-      message: t(TranslationNames.createClientForm.validation.emailPattern),
-    },
-    required: t(TranslationNames.createClientForm.validation.email),
-    minLength: {
-      value: LoginUserConstants.EMAIL_MIN_LEN,
-      message: creteMinLenMessage(LoginUserConstants.EMAIL_MIN_LEN, 'email'),
-    },
-    maxLength: {
-      value: LoginUserConstants.EMAIL_MAX_LEN,
-      message: creteMinLenMessage(LoginUserConstants.EMAIL_MAX_LEN, 'email'),
-    },
-  },
+  email: CommonRules.email,
   name: {
     required: t(TranslationNames.createClientForm.validation.name),
     minLength: {
@@ -74,93 +59,5 @@ export const rules: FormRulesType<CreateClientForm> = {
       ),
     },
   },
-  city: {
-    required: t(TranslationNames.addressForm.validation.city),
-    minLength: {
-      value: AddressConstants.CITY_MIN_LEN,
-      message: creteMinLenMessage(AddressConstants.CITY_MIN_LEN, 'city'),
-    },
-    maxLength: {
-      value: AddressConstants.CITY_MAX_LEN,
-      message: creteMinLenMessage(AddressConstants.CITY_MIN_LEN, 'city'),
-    },
-  },
-  county: {
-    required: t(TranslationNames.addressForm.validation.county),
-    minLength: {
-      value: AddressConstants.COUNTY_MIN_LEN,
-      message: creteMinLenMessage(AddressConstants.COUNTY_MIN_LEN, 'county'),
-    },
-    maxLength: {
-      value: AddressConstants.COUNTY_MAX_LEN,
-      message: creteMinLenMessage(AddressConstants.COUNTY_MAX_LEN, 'county'),
-    },
-  },
-  apartmentNumber: {
-    minLength: {
-      value: AddressConstants.APARTMENT_NR_MIN_LEN,
-      message: creteMinLenMessage(
-        AddressConstants.APARTMENT_NR_MIN_LEN,
-        'apartmentNumber',
-      ),
-    },
-    maxLength: {
-      value: AddressConstants.APARTMENT_NR_MAX_LEN,
-      message: creteMinLenMessage(
-        AddressConstants.APARTMENT_NR_MAX_LEN,
-        'apartmentNumber',
-      ),
-    },
-  },
-  houseNumber: {
-    required: t(TranslationNames.addressForm.validation.houseNumber),
-    minLength: {
-      value: AddressConstants.HOUSE_NR_MIN_LEN,
-      message: creteMinLenMessage(
-        AddressConstants.HOUSE_NR_MIN_LEN,
-        'houseNumber',
-      ),
-    },
-    maxLength: {
-      value: AddressConstants.HOUSE_NR_MAX_LEN,
-      message: creteMinLenMessage(
-        AddressConstants.HOUSE_NR_MAX_LEN,
-        'houseNumber',
-      ),
-    },
-  },
-  postalCode: {
-    required: t(TranslationNames.addressForm.validation.postalCode),
-    pattern: {
-      value: /^[0-9]{2}-[0-9]{3}$/,
-      message: t(TranslationNames.addressForm.validation.postalCode),
-    },
-  },
-  street: {
-    minLength: {
-      value: AddressConstants.STREET_MIN_LEN,
-      message: creteMinLenMessage(AddressConstants.STREET_MIN_LEN, 'street'),
-    },
-    maxLength: {
-      value: AddressConstants.STREET_MAX_LEN,
-      message: creteMinLenMessage(AddressConstants.STREET_MAX_LEN, 'street'),
-    },
-  },
-  voivodeship: {
-    required: t(TranslationNames.addressForm.validation.voivodeship),
-    minLength: {
-      value: AddressConstants.VOIVODESHIP_MIN_LEN,
-      message: creteMinLenMessage(
-        AddressConstants.VOIVODESHIP_MIN_LEN,
-        'voivodeship',
-      ),
-    },
-    maxLength: {
-      value: AddressConstants.VOIVODESHIP_MAX_LEN,
-      message: creteMinLenMessage(
-        AddressConstants.VOIVODESHIP_MAX_LEN,
-        'voivodeship',
-      ),
-    },
-  },
+  ...AddressRules,
 };
