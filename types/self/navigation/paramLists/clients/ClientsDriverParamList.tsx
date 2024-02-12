73824fb5027bd6f34d0feb76154eb9c3ab005a7c
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-12 07:35:51 +0100

    added - typing

--- types/self/navigation/paramLists/clients/ClientsDriverParamList.tsx
+++ types/self/navigation/paramLists/clients/ClientsDriverParamList.tsx
@@ -2,6 +2,7 @@ import { NavigatorScreenParams } from '@react-navigation/native';
 import { ClientsDesktopDriverParamList } from './ClientsDesktopDriverParamList';
 import { ClientResponseBase } from '../../../../../FarmServiceApiTypes/Clients/Responses';
 import { CreateClientReqI } from '../../../../../FarmServiceApiTypes/Clients/Requests';
+import { ClientsCompanyResponseBase } from '../../../../../FarmServiceApiTypes/ClientsCompany/Responses';
 
 export type ClientsDriverParamList = {
   clientsDesktopRoot: NavigatorScreenParams<ClientsDesktopDriverParamList>;
@@ -9,7 +10,11 @@ export type ClientsDriverParamList = {
     client: ClientResponseBase;
   };
   assignCompanyToClient: {
-    client?: CreateClientReqI;
-    editClient?: ClientResponseBase;
+    onCreateClient?: CreateClientReqI;
+    afterCreateClient?: ClientResponseBase;
+    onEdit?: {
+      client: ClientResponseBase;
+      company: ClientsCompanyResponseBase;
+    };
   };
 };
