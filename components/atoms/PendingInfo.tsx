3bdaeb36adbcf3a2fdd9df926ea1c96117dd8357
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-15 12:36:00 +0100

    updated - changed component to use global theme color and added sizing

--- components/atoms/PendingInfo.tsx
+++ components/atoms/PendingInfo.tsx
@@ -1,20 +1,28 @@
-import { ActivityIndicator, Text } from 'react-native';
+import { ActivityIndicator, ActivityIndicatorProps } from 'react-native';
 import { useEffect } from 'react';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
-import { Colors } from '../../settings/styles/colors';
+import { SizableText, useTheme } from 'tamagui';
 
 export type PendingInfoProps = {
-  infoText?: string;
   isVisible: boolean;
+  infoText?: string;
+  column?: boolean;
+  size?: ActivityIndicatorProps['size'];
 };
 
-export function PendingInfo({ infoText, isVisible }: PendingInfoProps) {
+export function PendingInfo({
+  infoText,
+  isVisible,
+  column,
+  size,
+}: PendingInfoProps) {
   const opacity = useSharedValue(0);
   const animatedStyle = useAnimatedStyle(() => ({ opacity: opacity.value }));
+  const { color } = useTheme();
 
   useEffect(() => {
     if (isVisible) opacity.value = withTiming(1, { duration: 200 });
@@ -26,10 +34,17 @@ export function PendingInfo({ infoText, isVisible }: PendingInfoProps) {
   return (
     <Animated.View
       style={[animatedStyle]}
-      className="flex-1 min-w-[100%] flex-row items-center justify-center"
+      className={`flex-1 min-w-[100%] ${
+        column ? 'flex-col' : 'flex-row'
+      } items-center justify-center`}
     >
-      <ActivityIndicator color={Colors.GREEN} />
-      <Text className="ml-2 text-dark dark:text-green">{infoText}</Text>
+      <ActivityIndicator size={size} color={color?.val} />
+      <SizableText
+        className={`ml-2 text-dark dark:text-green ${column && 'mt-4'}`}
+        size={(size === 'large' && '$5') || '$4'}
+      >
+        {infoText}
+      </SizableText>
     </Animated.View>
   );
 }
