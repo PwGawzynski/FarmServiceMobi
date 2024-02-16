41771a8f7af2aedf858201fa525baa62e62d9baa
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-16 16:02:35 +0100

    added fetchLog to store and created interface for store

--- src/redux/app/Store.ts
+++ src/redux/app/Store.ts
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import UserReducer from '../feature/userSlice';
+import UserReducer, { UserContextI } from '../feature/userSlice';
+import CachingDriverSlice, {
+  CachingDriverI,
+} from '../feature/cachingDriverSlice';
+
+export type MainStoreI = {
+  user: UserContextI | undefined;
+  queryFetchLogs: CachingDriverI;
+};
 
 const store = configureStore({
   reducer: {
     user: UserReducer,
+    queryFetchLogs: CachingDriverSlice,
   },
 });
 export default store;
