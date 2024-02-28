3599bb95352a53970c5916c057605df71af2d2ed
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-28 21:27:29 +0100

    created worker navigators, props, paramLists

---
+++ types/self/navigation/Worker/paramList/activities/WorkerActivityDriverParamList.tsx
@@ -0,0 +1,6 @@
+import { NavigatorScreenParams } from '@react-navigation/native';
+import { WorkerActivityDesktopDriverParamList } from './WorkerActivityDesktopDriverParamList';
+
+export type WorkerActivityDriverParamList = {
+  workerActivityDesktopRoot: NavigatorScreenParams<WorkerActivityDesktopDriverParamList>;
+};
