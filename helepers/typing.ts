7a84a6be7daae8992d2b188f0e4683cfac2e0dba
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-13 18:50:49 +0100

    added type

---
+++ helepers/typing.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AsAny<T extends { [key: string]: any }> = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [P in keyof T]: any;
+};
