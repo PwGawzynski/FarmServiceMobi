94c763b88567a55a53c3fbcdd1c5e03de988c91b
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-21 15:59:53 +0100

    updated filed li element to connect w bottomSheet

--- components/pages/owner/mobi/ClientsDriver/clientFields.tsx
+++ components/pages/owner/mobi/ClientsDriver/clientFields.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { YStack } from 'tamagui';
-import { useCallback } from 'react';
+import { RefObject, useCallback, useRef } from 'react';
 import { ListRenderItemInfo } from '@shopify/flash-list';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { ScreenBase } from '../common/ScreenBase';
 import { getClientFields } from '../../../../../api/clients/Client';
 import { ClientsDriverScreenProps } from '../../../../../types/self/navigation/Owner/props/clients/ClientsDriverProps';
@@ -9,6 +10,9 @@ import { EXPO_PUBLIC_QUERY_STALE_TIME } from '../../../../../settings/query/quer
 import FieldListItem from '../../../../molecules/FieldListItem';
 import { UniversalList } from '../../../../organisms/UniversalList';
 import { FieldResponseBase } from '../../../../../FarmServiceApiTypes/Field/Ressponses';
+import { ButtonTamagui } from '../../../../atoms/ButtonTamagui';
+import PlusIco from '../../../../../assets/plus.svg';
+import { FieldBottomSheetContent } from '../../../../molecules/FieldBottomSheetContent';
 
 export function ClientFields({
   route,
@@ -26,16 +30,46 @@ export function ClientFields({
   console.log(data);
 
   const renderItem = useCallback(
-    ({ item }: ListRenderItemInfo<FieldResponseBase>) => {
-      return <FieldListItem field={item} />;
+    (
+      { item }: ListRenderItemInfo<FieldResponseBase>,
+      bottomSheetRef: RefObject<BottomSheetModal>,
+    ) => {
+      return (
+        <FieldListItem
+          onPress={() => {
+            bottomSheetRef.current?.present(
+              <FieldBottomSheetContent field={item} />,
+            );
+          }}
+          field={item}
+        />
+      );
     },
     [],
   );
-
+  const modalRef = useRef<BottomSheetModal>(null);
   return (
-    <ScreenBase name="fields">
+    <ScreenBase
+      name="fields"
+      bottomSheetsProps={{
+        modalRef,
+        snapPoints: ['50%', '70%'],
+      }}
+    >
       <YStack mt="$4" f={1}>
-        <UniversalList listEmptyText="em" renderItem={renderItem} data={data} />
+        <UniversalList
+          renderItem={item => renderItem(item, modalRef)}
+          data={data}
+        />
+      </YStack>
+      <YStack mb="$2">
+        <ButtonTamagui
+          icon={<PlusIco />}
+          text="Add Filed"
+          buttonProps={{
+            onPress: () => modalRef?.current?.expand(),
+          }}
+        />
       </YStack>
     </ScreenBase>
   );
