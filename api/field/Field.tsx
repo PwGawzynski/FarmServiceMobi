d985c35fcba4d4e4fb6ef01c76f0767c58714cea
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-27 14:33:42 +0100

    added service

--- api/field/Field.tsx
+++ api/field/Field.tsx
@@ -2,7 +2,10 @@ import { t } from 'i18next';
 import { TranslationNames } from '../../locales/TranslationNames';
 import { apiHandler } from '../services/User';
 import { Api } from '../Api';
-import { CreateFieldReqI } from '../../FarmServiceApiTypes/Field/Requests';
+import {
+  CreateFieldReqI,
+  updateFieldReqI,
+} from '../../FarmServiceApiTypes/Field/Requests';
 import { FieldResponseBase } from '../../FarmServiceApiTypes/Field/Ressponses';
 
 export async function createField(data: CreateFieldReqI) {
@@ -12,6 +15,16 @@ export async function createField(data: CreateFieldReqI) {
     UNAUTHORIZED_MSG,
     DEFAULT_MSG,
     Api.createField,
+    data,
+  ) as unknown as FieldResponseBase | undefined;
+}
+export async function editField(data: updateFieldReqI) {
+  const UNAUTHORIZED_MSG = t(TranslationNames.serviceDefaults.unauthorised);
+  const DEFAULT_MSG = t(TranslationNames.serviceDefaults.default);
+  return apiHandler<updateFieldReqI, FieldResponseBase | undefined>(
+    UNAUTHORIZED_MSG,
+    DEFAULT_MSG,
+    Api.editField,
     data,
   ) as unknown as FieldResponseBase | undefined;
 }
