4165b71a22c3765e495ce04ec446215f630343a1
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-04 19:09:49 +0200

    added text area component for form

---
+++ components/molecules/TextArea.tsx
@@ -0,0 +1,56 @@
+import { TextArea as TA } from 'tamagui';
+import {
+  NativeSyntheticEvent,
+  TextInputTextInputEventData,
+} from 'react-native';
+import { RefCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { ValidationError } from '../atoms/ValidationError';
+import { selectTheme } from '../../src/redux/feature/userSlice';
+import { Theme } from '../../FarmServiceApiTypes/Account/Constants';
+
+export interface TextAreaProps {
+  onChange: (e: NativeSyntheticEvent<TextInputTextInputEventData>) => void;
+  onBlur: () => void;
+  value: string;
+  placeholderName?: string;
+  error: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ref: RefCallback<any>;
+  disabled?: boolean;
+}
+
+export function TextArea({
+  onChange,
+  onBlur,
+  value,
+  placeholderName,
+  error,
+  ref,
+  disabled,
+}: TextAreaProps) {
+  const theme = useSelector(selectTheme);
+  return (
+    <>
+      <TA
+        padding={10}
+        className="mt-4 text-base"
+        onTextInput={onChange}
+        borderColor={error ? '$color7' : '$color4'}
+        placeholderTextColor={theme === Theme.dark ? '$color11' : '$color9'}
+        onBlur={onBlur}
+        focusStyle={{
+          borderColor: error ? '$color7' : '$color4',
+          borderWidth: 2,
+        }}
+        value={value?.toString()}
+        placeholder={placeholderName}
+        backgroundColor="$color6"
+        height={120}
+        ref={ref}
+        disabled={disabled}
+      />
+      <ValidationError>{error}</ValidationError>
+    </>
+  );
+}
