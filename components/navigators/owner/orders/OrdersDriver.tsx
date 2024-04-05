fc3a9795be575ea08fa6ef5091cf2971946a4d81
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-05 11:21:45 +0200

    added screen

--- components/navigators/owner/orders/OrdersDriver.tsx
+++ components/navigators/owner/orders/OrdersDriver.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeStackScreenOptionsBase } from '../../../../settings/navigators/NativeStackScreenOptionsBase';
 import { OrdersDesktopDriver } from './OrdersDesktopDriver';
 import { OrdersDriverParamList } from '../../../../types/self/navigation/Owner/paramLists/orders/OrdersDriverParamList';
+import { OrderDetails } from '../../../pages/owner/mobi/OrderDriver/OrderDetails';
 
 const Stack = createNativeStackNavigator<OrdersDriverParamList>();
 
@@ -12,6 +13,14 @@ export function OrdersDriver() {
       initialRouteName="ordersDesktopRoot"
     >
       <Stack.Screen name="ordersDesktopRoot" component={OrdersDesktopDriver} />
+      <Stack.Screen
+        options={{
+          animation: 'default',
+          gestureDirection: 'horizontal',
+        }}
+        name="orderDetails"
+        component={OrderDetails}
+      />
     </Stack.Navigator>
   );
 }
