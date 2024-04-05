a2c1296b6c70d29e89a7effd9914079d1f6413de
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-05 10:18:35 +0200

    created base order desktop

--- components/pages/owner/mobi/OrdersDesktopDriver/OrderDesktop.tsx
+++ components/pages/owner/mobi/OrdersDesktopDriver/OrderDesktop.tsx
@@ -1,11 +1,21 @@
+import { useRef } from 'react';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { ScreenBase } from '../common/ScreenBase';
+import { OrderList } from '../../../../organisms/OrdersList';
 
 export function OrderDesktop() {
   // const company = useSelector(selectUserCompany);
+  const modalRef = useRef<BottomSheetModal>(null);
+
   return (
-    <ScreenBase name="orders">
-      {/* <View className="h-4 w-full" /> */}
-      {/* <OrdersList /> */}
+    <ScreenBase
+      name="orders"
+      bottomSheetsProps={{
+        modalRef,
+        snapPoints: ['50%', '70%'],
+      }}
+    >
+      <OrderList bottomSheetRef={modalRef} />
     </ScreenBase>
   );
 }
