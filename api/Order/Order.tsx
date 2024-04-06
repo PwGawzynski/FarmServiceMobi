fe81294a3bc86867b296f98a7e8a6cde94bc09d4
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-06 11:38:28 +0200

    added update order endpoint call

--- api/Order/Order.tsx
+++ api/Order/Order.tsx
@@ -3,7 +3,10 @@ import { TranslationNames } from '../../locales/TranslationNames';
 import { apiHandler } from '../services/User';
 import { Api } from '../Api';
 import { ResponseObject } from '../../FarmServiceApiTypes/Respnse/responseGeneric';
-import { CreateOrderReqI } from '../../FarmServiceApiTypes/Order/Requests';
+import {
+  CreateOrderReqI,
+  UpdateOrder,
+} from '../../FarmServiceApiTypes/Order/Requests';
 import { OrderResponseBase } from '../../FarmServiceApiTypes/Order/Ressponses';
 
 export async function createOrder(data: CreateOrderReqI) {
@@ -25,4 +28,15 @@ export async function getAllOrders() {
     DEFAULT_MSG,
     Api.getAllOrders,
   ) as unknown as Array<OrderResponseBase> | undefined;
+}
+
+export async function updateOrder(data: UpdateOrder) {
+  const UNAUTHORIZED_MSG = t(TranslationNames.serviceDefaults.unauthorised);
+  const DEFAULT_MSG = t(TranslationNames.serviceDefaults.default);
+  return apiHandler<UpdateOrder>(
+    UNAUTHORIZED_MSG,
+    DEFAULT_MSG,
+    Api.updateOrder,
+    data,
+  ) as unknown as OrderResponseBase | undefined;
 }
