f77dc813cbde30b8b07adb722d504ce6db5e4a7d
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-06 20:49:39 +0200

    added types

--- api/Order/Order.tsx
+++ api/Order/Order.tsx
@@ -2,7 +2,6 @@ import { t } from 'i18next';
 import { TranslationNames } from '../../locales/TranslationNames';
 import { apiHandler } from '../services/User';
 import { Api } from '../Api';
-import { ResponseObject } from '../../FarmServiceApiTypes/Respnse/responseGeneric';
 import {
   CreateOrderReqI,
   UpdateOrder,
@@ -17,7 +16,7 @@ export async function createOrder(data: CreateOrderReqI) {
     DEFAULT_MSG,
     Api.createOrder,
     data,
-  ) as unknown as ResponseObject<OrderResponseBase>;
+  ) as unknown as OrderResponseBase | undefined;
 }
 
 export async function getAllOrders() {
