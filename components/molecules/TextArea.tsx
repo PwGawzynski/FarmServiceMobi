dac78a42102fb47492e701b3706c09b2149956be
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-06 11:41:21 +0200

    created app text area component

--- components/molecules/TextArea.tsx
+++ components/molecules/TextArea.tsx
@@ -1,21 +1,20 @@
 import { TextArea as TA } from 'tamagui';
-import {
-  NativeSyntheticEvent,
-  TextInputTextInputEventData,
-} from 'react-native';
 import { useSelector } from 'react-redux';
+import { TextInputProps } from 'react-native';
 import { ValidationError } from '../atoms/ValidationError';
 import { selectTheme } from '../../src/redux/feature/userSlice';
 import { Theme } from '../../FarmServiceApiTypes/Account/Constants';
 
 export interface TextAreaProps {
-  onChange: (e: NativeSyntheticEvent<TextInputTextInputEventData>) => void;
+  onChange: (e: string) => void;
   onBlur: () => void;
   value: string;
   placeholderName?: string;
   error: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   disabled?: boolean;
+  textInputProps?: TextInputProps;
+  abs?: string;
 }
 
 export function TextArea({
@@ -25,16 +24,21 @@ export function TextArea({
   placeholderName,
   error,
   disabled,
+  textInputProps,
+  abs,
 }: TextAreaProps) {
   const theme = useSelector(selectTheme);
   return (
     <>
       <TA
         padding={10}
-        className="mt-4 text-base"
-        onTextInput={onChange}
+        className={`mt-4 text-base ${abs}`}
+        onChangeText={onChange}
         borderColor={error ? '$color7' : '$color4'}
-        placeholderTextColor={theme === Theme.dark ? '$color11' : '$color9'}
+        placeholderTextColor={
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (theme === Theme.dark ? '$color11' : '$color9') as any
+        }
         onBlur={onBlur}
         focusStyle={{
           borderColor: error ? '$color7' : '$color4',
@@ -45,6 +49,7 @@ export function TextArea({
         backgroundColor="$color6"
         height={120}
         disabled={disabled}
+        {...textInputProps}
       />
       <ValidationError>{error}</ValidationError>
     </>
