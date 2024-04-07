374df6e5172a5d884e378e5e5d154b7f4db4049a
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-07 21:19:39 +0200

    added params

--- types/self/navigation/Owner/paramLists/machines/MachinesDriverParamList.tsx
+++ types/self/navigation/Owner/paramLists/machines/MachinesDriverParamList.tsx
@@ -1,6 +1,10 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
 import { MachinesDesktopDriverParamList } from './MachinesDesktopDriverParamList';
+import { MachineResponseBase } from '../../../../../../FarmServiceApiTypes/Machine/Responses';
 
 export type MachinesDriverParamList = {
   machinesDesktopRoot: NavigatorScreenParams<MachinesDesktopDriverParamList>;
+  machineDetails: {
+    machine: MachineResponseBase;
+  };
 };
