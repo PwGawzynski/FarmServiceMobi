d69742170f00e1758f6d79543ce15bba0325ae4b
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-07 07:35:04 +0200

    created machine driver navigator param list

---
+++ types/self/navigation/Owner/paramLists/machines/MachinesDriverParamList.tsx
@@ -0,0 +1,6 @@
+import { NavigatorScreenParams } from '@react-navigation/native';
+import { MachinesDesktopDriverParamList } from './MachinesDesktopDriverParamList';
+
+export type MachinesDriverParamList = {
+  machinesDesktopRoot: NavigatorScreenParams<MachinesDesktopDriverParamList>;
+};
