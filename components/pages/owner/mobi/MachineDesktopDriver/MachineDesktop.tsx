6ad81a6506813170c9f79fdeec301f4bc8931d0e
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-07 22:23:19 +0200

    Changed list to be w search engine

--- components/pages/owner/mobi/MachineDesktopDriver/MachineDesktop.tsx
+++ components/pages/owner/mobi/MachineDesktopDriver/MachineDesktop.tsx
@@ -1,14 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import { YStack } from 'tamagui';
-import { useCallback } from 'react';
-import { ListRenderItemInfo } from '@shopify/flash-list';
+import { useRef } from 'react';
 import { t } from 'i18next';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { ScreenBase } from '../common/ScreenBase';
-import { getAllMachines } from '../../../../../api/Machine/Machine';
-import { UniversalList } from '../../../../organisms/UniversalList';
-import { MachineResponseBase } from '../../../../../FarmServiceApiTypes/Machine/Responses';
-import MachineListItem from '../../../../molecules/MachineListItem';
 import { TranslationNames } from '../../../../../locales/TranslationNames';
+import { MachineList } from '../../../../organisms/MachineList';
 
 const TRANSLATIONS = {
   screenTitle: t(
@@ -16,21 +11,16 @@ const TRANSLATIONS = {
   ),
 };
 export function MachineDesktop() {
-  const { data } = useQuery({
-    queryKey: ['machines'],
-    queryFn: getAllMachines,
-  });
-  const renderItem = useCallback(
-    ({ item }: ListRenderItemInfo<MachineResponseBase>) => {
-      return <MachineListItem machine={item} />;
-    },
-    [],
-  );
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
   return (
-    <ScreenBase name={TRANSLATIONS.screenTitle}>
-      <YStack mt="$4" f={1}>
-        <UniversalList renderItem={renderItem} data={data} />
-      </YStack>
+    <ScreenBase
+      bottomSheetsProps={{
+        modalRef: bottomSheetRef,
+        snapPoints: ['50%', '70%'],
+      }}
+      name={TRANSLATIONS.screenTitle}
+    >
+      <MachineList bottomSheetRef={bottomSheetRef} />
     </ScreenBase>
   );
 }
