59c8696c62b0f2dabb03035f8fa11ecdb05125e7
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-07 07:36:33 +0200

    added machine navigator

--- types/self/navigation/Owner/paramLists/OwnerRootDriverParamList.tsx
+++ types/self/navigation/Owner/paramLists/OwnerRootDriverParamList.tsx
@@ -3,10 +3,12 @@ import { ActivityDriverParamList } from './activities/ActivityDriverParamList';
 import { OrdersDriverParamList } from './orders/OrdersDriverParamList';
 import { ClientsDriverParamList } from './clients/ClientsDriverParamList';
 import { WorkersDriverParamList } from './workers/WorkersDriverParamList';
+import { MachinesDriverParamList } from './machines/MachinesDriverParamList';
 
 export type OwnerRootDriverParamList = {
   activityDriver: NavigatorScreenParams<ActivityDriverParamList>;
   ordersDriver: NavigatorScreenParams<OrdersDriverParamList>;
   clientsDriver: NavigatorScreenParams<ClientsDriverParamList>;
   workersDriver: NavigatorScreenParams<WorkersDriverParamList>;
+  machinesDriver: NavigatorScreenParams<MachinesDriverParamList>;
 };
