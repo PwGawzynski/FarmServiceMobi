2af3fb95acdea6d4c23e46d146a26db2ce1276fa
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-07 10:17:48 +0200

    added - machine desktop

--- components/pages/owner/mobi/MachineDesktopDriver/MachineDesktop.tsx
+++ components/pages/owner/mobi/MachineDesktopDriver/MachineDesktop.tsx
@@ -1,5 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
 import { ScreenBase } from '../common/ScreenBase';
+import { getAllMachines } from '../../../../../api/Machine/Machine';
 
 export function MachineDesktop() {
+  const { data } = useQuery({
+    queryKey: ['machines'],
+    queryFn: getAllMachines,
+  });
+  console.log(data);
   return <ScreenBase name="machine desktop" />;
 }
