d9942865f9e999c8d8333c748739dfadd7f2c81b
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-08 17:57:50 +0200

    added save delete option

--- components/pages/owner/mobi/MachineDriver/MachineDetails.tsx
+++ components/pages/owner/mobi/MachineDriver/MachineDetails.tsx
@@ -1,8 +1,17 @@
 import { t } from 'i18next';
+import { YStack } from 'tamagui';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import { ScreenBase } from '../common/ScreenBase';
 import { MachinesDriverScreenProps } from '../../../../../types/self/navigation/Owner/props/machines/MachinesDriverProps';
 import { EntityAsACard } from '../../../../molecules/EntityAsACard';
 import { TranslationNames } from '../../../../../locales/TranslationNames';
+import { ButtonTamagui } from '../../../../atoms/ButtonTamagui';
+import { Colors } from '../../../../../settings/styles/colors';
+import { safelyDeleteMachine } from '../../../../../api/Machine/Machine';
+import DangerIco from '../../../../../assets/danger.svg';
+import { MachineResponseBase } from '../../../../../FarmServiceApiTypes/Machine/Responses';
+import { showErrorToast } from '../../../../../helepers/ToastSetup';
 
 const TRANSLATIONS = {
   screenTitle: t(
@@ -28,6 +37,15 @@ const TRANSLATIONS = {
     TranslationNames.screens.machineDriver.machineDetailsScreen.entityAsACard
       .names.licensePlate,
   ),
+  deleteButton: t(
+    TranslationNames.screens.machineDriver.machineDetailsScreen.deleteButton,
+  ),
+  cantDeleteMachineHeader: t(
+    TranslationNames.components.toast.cantDeleteMachine,
+  ),
+  cantDeleteMachineDescription: t(
+    TranslationNames.components.toast.cantDeleteMachineDescription,
+  ),
 };
 
 export function MachineDetails({
@@ -39,6 +57,31 @@ export function MachineDetails({
   'ownerRootDriver'
 >) {
   const { machine } = params;
+
+  const queryClient = useQueryClient();
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationKey: ['machine', machine.id],
+    mutationFn: safelyDeleteMachine,
+    onSuccess: () => {
+      queryClient.setQueryData(['machines'], (old: MachineResponseBase[]) =>
+        old.filter((m: MachineResponseBase) => m.id !== machine.id),
+      );
+      navigation.navigate('machinesDriver', {
+        screen: 'machinesDesktopRoot',
+        params: { screen: 'machinesDesktop' },
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (isError)
+      showErrorToast(
+        error,
+        TRANSLATIONS.cantDeleteMachineHeader,
+        TRANSLATIONS.cantDeleteMachineDescription,
+      );
+  }, [isError]);
+
   const handleEditPress = () =>
     navigation.navigate('machinesDriver', {
       screen: 'machinesDesktopRoot',
@@ -47,19 +90,37 @@ export function MachineDetails({
         params: { machine },
       },
     });
+  const handleDeletePress = () =>
+    mutate({
+      machine: machine.id,
+      name: machine.name,
+      licensePlate: machine.licensePlate,
+    });
+
   return (
     <ScreenBase name={TRANSLATIONS.screenTitle}>
-      <EntityAsACard
-        onTopRightBtnPress={handleEditPress}
-        topRightButtonName={TRANSLATIONS.editButton}
-        cardName={TRANSLATIONS.cardName}
-        data={machine}
-        names={{
-          id: TRANSLATIONS.machineId,
-          name: TRANSLATIONS.machineName,
-          licensePlate: TRANSLATIONS.machineLicensePlate,
+      <YStack f={1}>
+        <EntityAsACard
+          onTopRightBtnPress={handleEditPress}
+          topRightButtonName={TRANSLATIONS.editButton}
+          cardName={TRANSLATIONS.cardName}
+          data={machine}
+          names={{
+            id: TRANSLATIONS.machineId,
+            name: TRANSLATIONS.machineName,
+            licensePlate: TRANSLATIONS.machineLicensePlate,
+          }}
+          omittedKeys={['id']}
+        />
+      </YStack>
+      <ButtonTamagui
+        icon={<DangerIco />}
+        bgColor={Colors.ERROR_RED}
+        text="Safely delete"
+        buttonProps={{
+          onPress: handleDeletePress,
+          disabled: isPending,
         }}
-        omittedKeys={['id']}
       />
     </ScreenBase>
   );
