10daf1c26aeb8f37ab2f58a73de51df481ab9637
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-10 22:55:56 +0200

    added screen props

--- types/self/navigation/Owner/paramLists/orders/OrdersDriverParamList.tsx
+++ types/self/navigation/Owner/paramLists/orders/OrdersDriverParamList.tsx
@@ -1,9 +1,11 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
 import { OrdersDesktopDriverParamList } from './OrdersDesktopDriverParamList';
 import { OrderResponseBase } from '../../../../../../FarmServiceApiTypes/Order/Ressponses';
+import { ClientResponseBase } from '../../../../../../FarmServiceApiTypes/Clients/Responses';
 
 export type OrdersDriverParamList = {
   ordersDesktopRoot: NavigatorScreenParams<OrdersDesktopDriverParamList>;
   orderDetails: { order: OrderResponseBase };
   editOrder: { order: OrderResponseBase };
+  createTask: { order: OrderResponseBase; client: ClientResponseBase };
 };
