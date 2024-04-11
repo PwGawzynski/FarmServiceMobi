1d980a451e84a72410bd90bda939b32c84473072
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-11 22:20:37 +0200

    adjust to be able to handle a select option

--- components/molecules/FieldListItem.tsx
+++ components/molecules/FieldListItem.tsx
@@ -1,31 +1,38 @@
 import { useNavigation } from '@react-navigation/native';
-import {
-  Card,
-  SizableText,
-  View,
-  XStack,
-  YStack,
-  Circle,
-  useTheme,
-} from 'tamagui';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withRepeat,
-  withSequence,
-  withTiming,
-} from 'react-native-reanimated';
-import { useEffect } from 'react';
+import { Card, SizableText, XStack, YStack, Circle, useTheme } from 'tamagui';
 import { FieldResponseBase } from '../../FarmServiceApiTypes/Field/Ressponses';
 import InfoIco from '../../assets/info.svg';
 
 export type Props = {
+  /**
+   * Fields to be displayed
+   */
   field: FieldResponseBase;
+  /**
+   * Callback triggered when the field is pressed
+   */
   onPress?: () => void;
+  /**
+   * Callback triggered when the field is pressed, navigates to given screen if onPress not provided
+   */
   onPressNavigateTo?: string;
+  /**
+   * Navigation params for onPressNavigateTo
+   */
   navigationParams?: Record<string, unknown>;
+  /**
+   * Callback when the field is selected, if this and onDeselect provided the field will be selectable
+   * @param field - the selected field
+   */
   onSelected?: (field: FieldResponseBase) => void;
+  /**
+   * Callback when the field is deselected, if this and onDeselect provided the field will be selectable
+   * @param field - the selected field
+   */
   onDeselected?: (field: FieldResponseBase) => void;
+  /**
+   * Determines if the field is selected
+   */
   isSelected?: boolean;
 };
 
@@ -43,9 +50,8 @@ export default function FieldListItem({
 
   const handlePress = () => {
     if (!onSelected && !onDeselected) {
-      if (!onPress && !onSelected)
-        navigation.navigate(onPressNavigateTo, navigationParams);
-      else if (!onSelected && onPress) onPress();
+      if (!onPress) navigation.navigate(onPressNavigateTo, navigationParams);
+      else if (onPress) onPress();
     }
     if (onSelected && !isSelected) {
       onSelected(field);
@@ -65,15 +71,17 @@ export default function FieldListItem({
   } = useTheme();
   return (
     <Card onPress={handlePress}>
-      <XStack p="$2" ai="center" justifyContent="flex-start">
-        <YStack>
-          <Circle height={25} width={25} bordered borderColor="$color11">
-            {isSelected && (
-              <Circle height={15} width={15} backgroundColor={val} />
-            )}
-          </Circle>
-        </YStack>
-        <YStack ml="$2">
+      <XStack f={1} p="$2" ai="center" justifyContent="flex-start">
+        {onSelected && onDeselected && (
+          <YStack>
+            <Circle height={25} width={25} bordered borderColor="$color11">
+              {isSelected && (
+                <Circle height={15} width={15} backgroundColor={val} />
+              )}
+            </Circle>
+          </YStack>
+        )}
+        <YStack width="75%" maxWidth="75%" ml="$2">
           <SizableText
             fontWeight="bold"
             fontSize={nameLabel.length > 20 ? '$4' : '$7'}
@@ -87,7 +95,8 @@ export default function FieldListItem({
           </SizableText>
           <XStack ml="$1" mt="$1" jc="space-between">
             <SizableText mr="$2" color="$color4" textAlign="center">
-              {city.toUpperCase()}
+              {city.substring(0, 10).toUpperCase() +
+                (city.length > 10 ? '...' : '')}
             </SizableText>
             <SizableText mr="$2" color="$color4" textAlign="right">
               ID:
@@ -109,46 +118,10 @@ export default function FieldListItem({
             </SizableText>
           </XStack>
         </YStack>
-        <YStack ai="flex-end" f={1}>
+        <YStack ai="flex-end" f={1} mr="$2">
           <InfoIco color={val} />
         </YStack>
       </XStack>
     </Card>
-  );
-}
-
-export interface ListItemSkeletonProps {
-  maxHeight?: number;
-}
-
-export function FieldListItemSkeleton({ maxHeight }: ListItemSkeletonProps) {
-  const DURATION = 800;
-  const opacity = useSharedValue(0);
-  const animatedStyle = useAnimatedStyle(() => ({ opacity: opacity.value }));
-  useEffect(() => {
-    opacity.value = withRepeat(
-      withSequence(
-        withTiming(1, { duration: DURATION }),
-        withTiming(0, { duration: DURATION }),
-      ),
-      -1,
-      true,
-    );
-  }, []);
-  return (
-    <View className={`flex-1 justify-center mt-2 mb-2 p-2 max-h-${maxHeight}`}>
-      <Animated.View style={[animatedStyle]} className="flex-1 flex-row">
-        <View className="flex-1 flex-col items-start">
-          <View backgroundColor="$color8" className="flex-1 rounded-md w-1/3" />
-          <View
-            backgroundColor="$color8"
-            className="flex-1 w-2/3 rounded-md mt-2"
-          />
-        </View>
-        <View className="justify-center">
-          <Circle height={25} width={25} backgroundColor="$color8" />
-        </View>
-      </Animated.View>
-    </View>
   );
 }
