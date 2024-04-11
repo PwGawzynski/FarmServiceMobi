a8c6f00c6dbf2640068dc4cc1f69dfeda91f2ddc
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-11 22:18:19 +0200

    select fields done

--- components/pages/owner/mobi/OrderDriver/CreateTask.tsx
+++ components/pages/owner/mobi/OrderDriver/CreateTask.tsx
@@ -1,37 +1,65 @@
-import { useRef, useState } from 'react';
+import { RefObject, useRef, useState } from 'react';
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { YStack } from 'tamagui';
-import { useQuery } from '@tanstack/react-query';
 import { ScreenBase } from '../common/ScreenBase';
 import { OrdersDriverScreenProps } from '../../../../../types/self/navigation/Owner/props/orders/OrdersDriverProps';
-import { FieldResponseBase } from '../../../../../FarmServiceApiTypes/Field/Ressponses';
-import { ClientFieldsList } from '../../../../organisms/ClientFieldsList';
+import ClientFieldsList, {
+  ClientFieldsListRef,
+} from '../../../../organisms/ClientFieldsList';
 import { GuideCard, GuideCardElement } from '../../../../atoms/GuideCard';
 import { ButtonTamagui } from '../../../../atoms/ButtonTamagui';
-import { getClientFields } from '../../../../../api/clients/Client';
-import { EXPO_PUBLIC_QUERY_STALE_TIME } from '../../../../../settings/query/querySettings';
-import SelectAll from '../../../../../assets/list-checks.svg';
+import { ClientResponseBase } from '../../../../../FarmServiceApiTypes/Clients/Responses';
+
+enum ScreenState {
+  SelectField,
+  SelectWorker,
+  SelectMachine,
+}
+
+interface SelectFieldsProps {
+  client: ClientResponseBase;
+  modalRef: RefObject<BottomSheetModal>;
+  setScreenState: React.Dispatch<React.SetStateAction<ScreenState>>;
+}
+
+function SelectFields({ client, modalRef, setScreenState }: SelectFieldsProps) {
+  const fieldListRef = useRef<ClientFieldsListRef>(null);
+  const [canSubmit, setCanSubmit] = useState(false);
+  return (
+    <YStack f={1}>
+      <ClientFieldsList
+        isSelectable
+        triggerOnSelectedChange={isEmpty => {
+          setCanSubmit(!isEmpty);
+        }}
+        ref={fieldListRef}
+        client={client}
+        modalRef={modalRef}
+      />
+      {canSubmit && (
+        <ButtonTamagui
+          text="Create Task"
+          buttonProps={{
+            onPress: () => {
+              setScreenState(ScreenState.SelectWorker);
+              console.log(fieldListRef.current?.fields);
+            },
+          }}
+        />
+      )}
+    </YStack>
+  );
+}
 
 export function CreateTask({
   route: { params },
 }: OrdersDriverScreenProps<'createTask', 'ordersDriver', 'ownerRootDriver'>) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { order, client } = params;
-  const [fields, setFields] = useState<FieldResponseBase[] | []>([]);
+
   const modalRef = useRef<BottomSheetModal>(null);
-  const [selectAll, setSelectAll] = useState(false);
 
-  const { data } = useQuery({
-    queryKey: ['clientFields', client.id],
-    queryFn: keys => getClientFields(keys.queryKey[1] as string),
-    staleTime: EXPO_PUBLIC_QUERY_STALE_TIME,
-  });
-
-  const handleFieldSelection = (field: FieldResponseBase) =>
-    setFields(p => [...p, field]);
-  const handleFieldDeselection = (field: FieldResponseBase) =>
-    setFields(prevState => prevState.filter(f => f.id !== field.id));
-
+  const [screenState, setScreenState] = useState(ScreenState.SelectField);
   return (
     <ScreenBase
       name="createTask"
@@ -40,53 +68,33 @@ export function CreateTask({
         snapPoints: ['50%', '70%'],
       }}
     >
-      {!(fields.length > 100) && (
-        <>
-          <YStack className="mt-2 ">
-            <YStack className="mt-2">
-              <GuideCard
-                header="Hint"
-                text="You can choose multiple fields, each field will be considered as a separate task"
-              >
-                <GuideCardElement isDone text="Select fields" />
-                <GuideCardElement isCurent text="Select worker" />
-                <GuideCardElement text="Select machine" />
-              </GuideCard>
-            </YStack>
-          </YStack>
-          <ClientFieldsList
-            fields={fields}
-            onItemSelected={handleFieldSelection}
-            onItemDeselected={handleFieldDeselection}
+      <YStack f={1} className="mt-2">
+        <YStack className="mt-2">
+          <GuideCard
+            header="Hint"
+            text="You can choose multiple fields, each field will be considered as a separate task"
+          >
+            <GuideCardElement
+              isCurent={screenState === ScreenState.SelectField}
+              isDone={screenState !== ScreenState.SelectField}
+              text="Select fields"
+            />
+            <GuideCardElement
+              isCurent={screenState === ScreenState.SelectWorker}
+              isDone={screenState !== ScreenState.SelectField}
+              text="Select worker"
+            />
+            <GuideCardElement text="Select machine" />
+          </GuideCard>
+        </YStack>
+        {screenState === ScreenState.SelectField && (
+          <SelectFields
             client={client}
             modalRef={modalRef}
+            setScreenState={setScreenState}
           />
-          <ButtonTamagui
-            text={selectAll ? 'Deselect all' : 'Select all'}
-            icon={<SelectAll />}
-            buttonProps={{
-              onPress: () => {
-                if (!selectAll) {
-                  setFields(data as FieldResponseBase[]);
-                  setSelectAll(true);
-                } else {
-                  setFields([]);
-                  setSelectAll(false);
-                }
-              },
-            }}
-          />
-          {fields.length > 0 && (
-            <ButtonTamagui
-              text={selectAll ? 'Deselect all' : 'Select all'}
-              icon={<SelectAll />}
-              buttonProps={{
-                onPress: () => {},
-              }}
-            />
-          )}
-        </>
-      )}
+        )}
+      </YStack>
     </ScreenBase>
   );
 }
