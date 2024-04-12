fc5798b7bfc58936a844bc14b11a2a3c568dbf3a
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-12 18:28:08 +0200

    added bottom sheet ref

--- components/pages/owner/mobi/WorkersDesktopDriver/WorkersDesktop.tsx
+++ components/pages/owner/mobi/WorkersDesktopDriver/WorkersDesktop.tsx
@@ -1,14 +1,20 @@
 import { t } from 'i18next';
+import { useRef } from 'react';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { ScreenBase } from '../common/ScreenBase';
 import { TranslationNames } from '../../../../../locales/TranslationNames';
 import { WorkerList } from '../../../../organisms/WorkerList';
 
 export function WorkersDesktop() {
+  const modalRef = useRef<BottomSheetModal>(null);
   return (
     <ScreenBase
       name={t(TranslationNames.screens.ownerRootDriver.workersDesktop.title)}
+      bottomSheetsProps={{
+        modalRef,
+      }}
     >
-      <WorkerList isSelectable />
+      <WorkerList modalRef={modalRef} />
     </ScreenBase>
   );
 }
