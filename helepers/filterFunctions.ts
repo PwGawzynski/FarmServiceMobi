63fe36b123d5188543bf3399d01f8966264da786
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-13 11:05:32 +0200

    moved all filters fn to  this file

--- helepers/filterFunctions.ts
+++ helepers/filterFunctions.ts
@@ -1,4 +1,7 @@
 import { FieldResponseBase } from '../FarmServiceApiTypes/Field/Ressponses';
+import { MachineResponseBase } from '../FarmServiceApiTypes/Machine/Responses';
+import { OrderResponseBase } from '../FarmServiceApiTypes/Order/Ressponses';
+import { ClientResponseBase } from '../FarmServiceApiTypes/Clients/Responses';
 
 export const searchEngineNameSurnameFilter = <
   T extends { personalData: { name: string; surname: string } },
@@ -35,4 +38,62 @@ export const clientFieldsFilter = (
     )
     .sort((a, b) =>
       a.nameLabel.toLowerCase() > b.nameLabel.toLowerCase() ? 1 : -1,
+    );
+
+export const machineFilter = (
+  data: MachineResponseBase[] | undefined,
+  filter: string | undefined,
+) =>
+  data
+    ?.filter(m =>
+      filter
+        ? (m.name + m.licensePlate)
+            .trim()
+            .replaceAll(' ', '')
+            .toLowerCase()
+            .includes(filter.toLowerCase().replaceAll(' ', ''))
+        : true,
+    )
+    .sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
+
+export const filterOrder = (
+  data: OrderResponseBase[] | undefined,
+  filter: string | undefined,
+) =>
+  data
+    ?.filter(order =>
+      filter
+        ? order.name
+            .trim()
+            .toLowerCase()
+            .includes(filter.toLowerCase().replace(' ', ''))
+        : true,
+    )
+    .sort((a, b) =>
+      new Date(a.performanceDate).getTime() >
+      new Date(b.performanceDate).getTime()
+        ? 1
+        : -1,
+    );
+
+export const clientListFilter = (
+  data: ClientResponseBase[] | undefined,
+  filter: string | undefined,
+) =>
+  data
+    ?.filter(client =>
+      filter
+        ? (
+            client.user.personal_data.name.trim() +
+            client.user.personal_data.surname.trim()
+          )
+            .toLowerCase()
+            .includes(filter.toLowerCase().replace(' ', ''))
+        : true,
+    )
+    .sort((a, b) =>
+      a.user.personal_data.surname.toLowerCase() >
+      b.user.personal_data.surname.toLowerCase()
+        ? 1
+        : -1,
     );
