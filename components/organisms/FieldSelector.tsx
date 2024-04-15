2f1a64e789b0d3bed0715c3b2010d354cc67c566
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-15 12:13:40 +0200

    created an optimized selectable field list

---
+++ components/organisms/FieldSelector.tsx
@@ -0,0 +1,71 @@
+import { ForwardedRef, RefObject, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { YStack } from 'tamagui';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { t } from 'i18next';
+import { getClientFields } from '../../api/clients/Client';
+import { EXPO_PUBLIC_QUERY_STALE_TIME } from '../../settings/query/querySettings';
+import List, { ListRef } from './List';
+import { FieldResponseBase } from '../../FarmServiceApiTypes/Field/Ressponses';
+import { createFieldBottomSign } from '../pages/owner/mobi/ClientsDriver/clientFields';
+import { clientFieldsFilter } from '../../helepers/filterFunctions';
+import { ButtonTamagui } from '../atoms/ButtonTamagui';
+import { ClientResponseBase } from '../../FarmServiceApiTypes/Clients/Responses';
+import { TranslationNames } from '../../locales/TranslationNames';
+
+export interface SelectFieldsProps {
+  client: ClientResponseBase;
+  modalRef: RefObject<BottomSheetModal>;
+  onSetAction: () => void;
+  fieldListRef: ForwardedRef<ListRef<FieldResponseBase>>;
+}
+const TRANSLATIONS = {
+  searchPlaceholder: t(
+    TranslationNames.components.fieldSelector.searchPlaceholder,
+  ),
+  next_step_button: t(TranslationNames.components.fieldSelector.subbmitButton),
+};
+export function FieldSelector({
+  client,
+  modalRef,
+  onSetAction,
+  fieldListRef,
+}: SelectFieldsProps) {
+  const [canSubmit, setCanSubmit] = useState(false);
+  const { data, isError, isFetching } = useQuery({
+    queryKey: ['clientFields', client.id],
+    queryFn: keys => getClientFields(keys.queryKey[1] as string),
+    staleTime: EXPO_PUBLIC_QUERY_STALE_TIME,
+  });
+  return (
+    <YStack f={1}>
+      <List<FieldResponseBase>
+        isSelectable
+        triggerOnSelectedChange={isEmpty => {
+          setCanSubmit(!isEmpty);
+        }}
+        ref={fieldListRef}
+        isFetching={isFetching}
+        isError={isError}
+        data={data}
+        modalRef={modalRef}
+        listStyleSettings={item => ({
+          header: item.nameLabel,
+          bottomRightText: createFieldBottomSign(item),
+          alignment: 'left',
+          infoIco: true,
+        })}
+        filterFunction={clientFieldsFilter}
+        searchEnginePlaceholder={TRANSLATIONS.searchPlaceholder}
+      />
+      {canSubmit && (
+        <ButtonTamagui
+          text={TRANSLATIONS.next_step_button}
+          buttonProps={{
+            onPress: () => onSetAction(),
+          }}
+        />
+      )}
+    </YStack>
+  );
+}
