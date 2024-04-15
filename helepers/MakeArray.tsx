2acd15753feab9d538f29e70e2a2347a3db91123
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-15 12:17:20 +0200

    added helper method

--- helepers/MakeArray.tsx
+++ helepers/MakeArray.tsx
@@ -4,3 +4,7 @@ export const makeArray = (e: EnumType) =>
   Object.keys(e)
     .map(key => e[key])
     .filter(_e => typeof _e === 'string') as Array<string>;
+export const findEnumVal = (e: EnumType, value: string) =>
+  Object.keys(e)
+    .filter(k => Number.isNaN(Number(k)))
+    .findIndex(key => key.toLowerCase() === value.toLowerCase());
