96664308bf1fb6b74251240422b9e78c5ac50ef1
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-16 10:32:55 +0200

    added flex option

--- components/atoms/ListInfo.tsx
+++ components/atoms/ListInfo.tsx
@@ -1,5 +1,5 @@
 import { Dimensions, View } from 'react-native';
-import { SizableText, useTheme } from 'tamagui';
+import { SizableText, useTheme, YStack } from 'tamagui';
 import React from 'react';
 
 export type ListEmptyProps = {
@@ -9,6 +9,7 @@ export type ListEmptyProps = {
   text?: string;
   children?: React.ReactElement | React.ReactElement[] | boolean;
   color?: string;
+  beFlex?: boolean;
 };
 
 export function ListInfo({
@@ -17,29 +18,33 @@ export function ListInfo({
   text,
   children,
   color: _color,
+  beFlex,
 }: ListEmptyProps) {
   const EMPTY_HEIGHT = height ?? Dimensions.get('window').height * 0.7;
   const { color } = useTheme();
   return (
     <View
       style={{
-        height: EMPTY_HEIGHT,
+        height: beFlex ? undefined : EMPTY_HEIGHT,
+        flex: beFlex ? 1 : undefined,
       }}
       className="flex-1 items-center justify-center"
     >
-      <Ico
-        width={EMPTY_HEIGHT / 10}
-        height={EMPTY_HEIGHT / 10}
-        color={_color ?? color?.val}
-      />
-      <SizableText
-        color={_color ?? color?.val}
-        className="mt-8  text-center"
-        fontSize="$5"
-      >
-        {text ?? 'Unknown Error Occurred!'}
-      </SizableText>
-      {children}
+      <YStack ai="center" jc="center" f={1}>
+        <Ico
+          width={EMPTY_HEIGHT / 10}
+          height={EMPTY_HEIGHT / 10}
+          color={_color ?? color?.val}
+        />
+        <SizableText
+          color={_color ?? color?.val}
+          className="mt-8  text-center"
+          fontSize="$5"
+        >
+          {text ?? 'Unknown Error Occurred!'}
+        </SizableText>
+        {children}
+      </YStack>
     </View>
   );
 }
