97f5a6e7ce25a0c97a463b7a2fe842d28eded662
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-19 22:50:00 +0200

    added TaskView screen

--- components/navigators/worker/activities/ActivitiesDriver.tsx
+++ components/navigators/worker/activities/ActivitiesDriver.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeStackScreenOptionsBase } from '../../../../settings/navigators/NativeStackScreenOptionsBase';
 import { ActivitiesDesktopDriver } from './ActivitiesDesktopDriver';
 import { WorkerActivityDriverParamList } from '../../../../types/self/navigation/Worker/paramList/activities/WorkerActivityDriverParamList';
+import { TaskView } from '../../../pages/worker/ActivityDriver/TaskView';
 
 const Stack = createNativeStackNavigator<WorkerActivityDriverParamList>();
 
@@ -14,6 +15,14 @@ export function ActivityDriver() {
       <Stack.Screen
         name="workerActivityDesktopRoot"
         component={ActivitiesDesktopDriver}
+      />
+      <Stack.Screen
+        options={{
+          animation: 'slide_from_right',
+          gestureDirection: 'horizontal',
+        }}
+        name="taskView"
+        component={TaskView}
       />
     </Stack.Navigator>
   );
