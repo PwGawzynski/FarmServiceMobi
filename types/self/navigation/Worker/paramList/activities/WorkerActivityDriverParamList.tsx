9149dce69da30622a30b2ef1eb63be35a3204f19
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-19 22:53:56 +0200

    added task view param list

--- types/self/navigation/Worker/paramList/activities/WorkerActivityDriverParamList.tsx
+++ types/self/navigation/Worker/paramList/activities/WorkerActivityDriverParamList.tsx
@@ -1,6 +1,10 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
 import { WorkerActivityDesktopDriverParamList } from './WorkerActivityDesktopDriverParamList';
+import { TaskResponseBase } from '../../../../../../FarmServiceApiTypes/Task/Responses';
 
 export type WorkerActivityDriverParamList = {
   workerActivityDesktopRoot: NavigatorScreenParams<WorkerActivityDesktopDriverParamList>;
+  taskView: {
+    task: TaskResponseBase;
+  };
 };
