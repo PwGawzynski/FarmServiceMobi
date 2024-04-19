8181dfce615d461de45705ad9b778ee2d0d59a7f
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-19 22:50:25 +0200

    added task list

--- components/pages/worker/ActivitiesDesktopDriver/ActivityDesktopRoot.tsx
+++ components/pages/worker/ActivitiesDesktopDriver/ActivityDesktopRoot.tsx
@@ -1,8 +1,20 @@
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { t } from 'i18next';
 import { ScreenBase } from '../../owner/mobi/common/ScreenBase';
 import { ActivitiesDesktopDriverScreenProps } from '../../../../types/self/navigation/Worker/props/activities/WorkerActivitiesDesktopDriverProps';
 import { selectUser } from '../../../../src/redux/feature/userSlice';
+import { Api } from '../../../../api/Api';
+import { TaskResponseBase } from '../../../../FarmServiceApiTypes/Task/Responses';
+import List from '../../../organisms/List';
+import { TaskType } from '../../../../FarmServiceApiTypes/Task/Enums';
+import { filterTasks } from '../../../../helepers/filterFunctions';
+import { TranslationNames } from '../../../../locales/TranslationNames';
+
+const TRANSLATIONS = {
+  welcome: t(TranslationNames.workerScreens.activityDesktopRoot.welcome),
+};
 
 export function ActivityDesktopRoot({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -15,12 +27,48 @@ export function ActivityDesktopRoot({
 >) {
   const user = useSelector(selectUser);
   const name = user?.personalData.name;
+  const [tasks, setTasks] = useState<TaskResponseBase[]>([]);
   useEffect(() => {
-    /* Api.workerTasks({
-      open: m => console.log(m),
-      message: m => console.log(m),
-      error: m => console.log(m),
-    }); */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let es: any;
+    (async () => {
+      const eventSource = await Api.workerTasks({
+        message: (m: TaskResponseBase[]) => setTasks(m),
+      });
+      es = eventSource;
+    })();
+    return () => {
+      es.removeAllEventListeners();
+      es.close();
+    };
   }, []);
-  return <ScreenBase name={`Welcome ${name}`} />;
+  const modalRef = useRef<BottomSheetModal>(null);
+  return (
+    <ScreenBase
+      bottomSheetsProps={{
+        modalRef,
+        snapPoints: ['50%', '70%'],
+      }}
+      name={`${TRANSLATIONS.welcome} ${name}`}
+    >
+      <List<TaskResponseBase>
+        isFetching={false}
+        isError={false}
+        isLoading={false}
+        data={tasks}
+        onPressNavigateTo="taskView"
+        navigationParamName="task"
+        modalRef={modalRef}
+        listStyleSettings={item => ({
+          header: item.field.nameLabel,
+          bottomRightText: `${TaskType[item.type]} ( ${new Date(
+            item.performanceDate,
+          ).toLocaleDateString()} )`,
+          alignment: 'left',
+          infoIco: true,
+        })}
+        filterFunction={filterTasks}
+      />
+    </ScreenBase>
+  );
 }
