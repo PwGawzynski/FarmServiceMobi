f4842e6d2d6f2f97ae52e51784e0c6306c2cfe67
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-22 22:59:31 +0200

    added screen base for worker

--- components/pages/owner/mobi/common/ScreenBase.tsx
+++ components/pages/owner/mobi/common/ScreenBase.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Platform, SafeAreaView, StatusBar, Text, View } from 'react-native';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { YStack } from 'tamagui';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  LinearTransition,
+} from 'react-native-reanimated';
 import {
   MainBottomSheet,
   MainBottomSheetProps,
@@ -11,6 +19,7 @@ export type Props = {
   children?: React.ReactNode;
   activityDot?: boolean;
   bottomSheetsProps?: MainBottomSheetProps;
+  switchOfMargins?: boolean;
 };
 
 export function ScreenBase({
@@ -18,6 +27,7 @@ export function ScreenBase({
   children,
   activityDot,
   bottomSheetsProps,
+  switchOfMargins,
 }: Props) {
   return (
     <SafeAreaView
@@ -27,12 +37,14 @@ export function ScreenBase({
       }}
     >
       <BottomSheetModalProvider>
-        <View className="flex-1 ml-4 mr-4 ">
-          <View className="w-full h-2  items-end">
-            {activityDot && (
-              <View className="h-2 w-2 rounded-full bg-activity-dot" />
-            )}
-          </View>
+        <View className={`flex-1 ${!switchOfMargins && 'ml-4 mr-4'}`}>
+          {activityDot && (
+            <View className="w-full h-2  items-end">
+              {activityDot && (
+                <View className="h-2 w-2 rounded-full bg-activity-dot" />
+              )}
+            </View>
+          )}
           {name && (
             <View className="w-full">
               <Text className="text-2xl uppercase font-bold text-dark dark:text-green">
@@ -45,5 +57,64 @@ export function ScreenBase({
         {bottomSheetsProps && <MainBottomSheet {...bottomSheetsProps} />}
       </BottomSheetModalProvider>
     </SafeAreaView>
+  );
+}
+
+export function ScreenBaseWithoutSaveArea({
+  name,
+  children,
+  activityDot,
+  bottomSheetsProps,
+  isModalVisible,
+}: Props & { isModalVisible?: boolean }) {
+  const bottomOffset = useSharedValue(0);
+  const transition = LinearTransition.duration(100).easing(Easing.linear);
+  const mbAnimation = useAnimatedStyle(() => ({
+    transform: [{ translateY: bottomOffset.value }],
+  }));
+  useEffect(() => {
+    if (isModalVisible) {
+      bottomOffset.value = withTiming(-100, {
+        duration: 100,
+      });
+    } else {
+      bottomOffset.value = withTiming(0, {
+        duration: 100,
+      });
+    }
+  }, [isModalVisible]);
+  return (
+    <YStack
+      className="flex-1 bg-[#fff] dark:bg-dark"
+      style={{
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <BottomSheetModalProvider>
+        <Animated.View
+          layout={transition}
+          style={[{ flex: 1, top: 0, left: 0 }, mbAnimation]}
+        >
+          <View className="flex-1">
+            {activityDot && (
+              <View className="w-full h-2  items-end">
+                {activityDot && (
+                  <View className="h-2 w-2 rounded-full bg-activity-dot" />
+                )}
+              </View>
+            )}
+            {name && (
+              <View className="w-full">
+                <Text className="text-2xl uppercase font-bold text-dark dark:text-green">
+                  {name}
+                </Text>
+              </View>
+            )}
+            {children}
+          </View>
+        </Animated.View>
+        {bottomSheetsProps && <MainBottomSheet {...bottomSheetsProps} />}
+      </BottomSheetModalProvider>
+    </YStack>
   );
 }
