added8fed5648d9c3f15ebd380afb24215eb286b
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-23 22:51:33 +0200

    added translations and queries

--- components/molecules/TaskWorkView.tsx
+++ components/molecules/TaskWorkView.tsx
@@ -1,15 +1,22 @@
 import { YStack } from 'tamagui';
 import { Dispatch, SetStateAction, useContext, useState } from 'react';
 import MapView from 'react-native-maps';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { t } from 'i18next';
+import Toast from 'react-native-toast-message';
 import { TaskResponseBase } from '../../FarmServiceApiTypes/Task/Responses';
 import { WorkerModalContext } from '../../helepers/context/WorkerModalContext';
 import { ButtonTamagui } from '../atoms/ButtonTamagui';
 import PauseIco from '../../assets/pause.svg';
 import EndIco from '../../assets/circle-check-big.svg';
 import { Switch } from '../atoms/Switch';
+import { closeTask, pauseTask } from '../../api/Task/Task';
+import { TranslationNames } from '../../locales/TranslationNames';
+import { updateWorkerTasks } from '../../helepers/FetchingHelpers';
 
 export interface TaskWorkViewProps {
   task: TaskResponseBase;
+  setTask: Dispatch<SetStateAction<TaskResponseBase>>;
   mapRef: React.RefObject<MapView>;
   onClosePress?: Dispatch<SetStateAction<boolean>>;
 }
@@ -26,39 +33,89 @@ export interface UserLocationI {
   isFromMockProvider?: boolean | undefined;
 }
 
+const TRANSLATIONS = {
+  pauseButton: t(TranslationNames.components.taskWorkView.pauseButton),
+  focusOnFieldButton: t(
+    TranslationNames.components.taskWorkView.focusOnFieldButton,
+  ),
+  autofocusSwitch: t(TranslationNames.components.taskWorkView.autofocusSwitch),
+  markAsDoneButton: t(
+    TranslationNames.components.taskWorkView.markAsDoneButton,
+  ),
+  errorToastTitleClose: t(
+    TranslationNames.components.taskWorkView.errorToastTitleClose,
+  ),
+  errorToastTitlePause: t(
+    TranslationNames.components.taskWorkView.errorToastTitlePause,
+  ),
+};
+
 export function TaskWorkView({
   task,
   mapRef,
   onClosePress,
+  setTask,
 }: TaskWorkViewProps) {
   const modal = useContext(WorkerModalContext);
   const [isAutofocused, setIsAutofocused] = useState(true);
+  const qc = useQueryClient();
+  const { mutate: close, isPending: isClosePending } = useMutation({
+    mutationKey: ['closeTask', task.id],
+    mutationFn: closeTask,
+    onSuccess: sth => {
+      setTask(sth);
+      onClosePress?.(false);
+      modal?.setIsModalVisible(false);
+      modal?.modalRef?.current?.close();
+      modal?.modalRef?.current?.dismiss();
+      updateWorkerTasks(qc, sth, task.id);
+    },
+    onError: e =>
+      Toast.show({
+        type: 'error',
+        text1: TRANSLATIONS.errorToastTitleClose,
+        text2: e.message,
+      }),
+  });
+
+  const { mutate: pause, isPending: isPausePending } = useMutation({
+    mutationKey: ['pauseTask', task.id],
+    mutationFn: pauseTask,
+    onSuccess: sth => {
+      setTask(sth);
+      onClosePress?.(false);
+      modal?.setIsModalVisible(false);
+      modal?.modalRef?.current?.close();
+      modal?.modalRef?.current?.dismiss();
+      updateWorkerTasks(qc, sth, task.id);
+    },
+    onError: e =>
+      Toast.show({
+        type: 'error',
+        text1: TRANSLATIONS.errorToastTitleClose,
+        text2: e.message,
+      }),
+  });
 
   return (
     <YStack f={1} m="$4" mt="0" mb="10%">
       <YStack>
         <ButtonTamagui
           icon={<PauseIco />}
+          isPending={isPausePending}
           buttonProps={{
-            onPress: () => {
-              onClosePress?.(false);
-              modal?.setIsModalVisible(false);
-              modal?.modalRef?.current?.close();
-            },
+            onPress: () => pause(task.id),
           }}
-          text="Pause"
+          text={TRANSLATIONS.pauseButton}
         />
         <ButtonTamagui
           icon={<EndIco />}
+          isPending={isClosePending}
           buttonProps={{
             mt: '$2',
-            onPress: () => {
-              onClosePress?.(false);
-              modal?.setIsModalVisible(false);
-              modal?.modalRef?.current?.close();
-            },
+            onPress: () => close(task.id),
           }}
-          text="Mark as done"
+          text={TRANSLATIONS.markAsDoneButton}
         />
         <ButtonTamagui
           icon={<EndIco />}
@@ -79,7 +136,7 @@ export function TaskWorkView({
               });
             },
           }}
-          text="Focus To Field"
+          text={TRANSLATIONS.focusOnFieldButton}
         />
       </YStack>
       <Switch
@@ -100,7 +157,7 @@ export function TaskWorkView({
           }
         }}
         checked={isAutofocused}
-        label="Autofocus"
+        label={TRANSLATIONS.autofocusSwitch}
       />
       <YStack f={1} />
     </YStack>
