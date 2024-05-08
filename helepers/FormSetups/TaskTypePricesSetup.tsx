4b5ecd7f5c7051383f2f2a8ddc5930a4fdb9108d
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-08 22:05:29 +0200

    created price setup

---
+++ helepers/FormSetups/TaskTypePricesSetup.tsx
@@ -0,0 +1,49 @@
+import { Control } from 'react-hook-form/dist/types/form';
+import { t } from 'i18next';
+import { FormControllerSetup } from '../../components/atoms/FormCreator';
+import { TaskTypePriceRules } from '../FormRules/TaskTypePriceRules';
+import { TranslationNames } from '../../locales/TranslationNames';
+import { OrderAccountingFormI } from '../../types/self/common/types';
+
+export const createTaskTypePriceSetup = (
+  control: Control<OrderAccountingFormI>,
+): FormControllerSetup<OrderAccountingFormI> => [
+  {
+    control,
+    rules: TaskTypePriceRules.Harvesting,
+    name: 'Harvesting',
+    placeholderName: t(
+      TranslationNames.taskTypePriceForm.formPlaceholder.harvesting,
+    ),
+    textInputProp: {
+      keyboardType: 'numeric',
+      placeholder: t(
+        TranslationNames.taskTypePriceForm.validation.priceRequired,
+      ),
+    },
+  },
+  {
+    control,
+    rules: TaskTypePriceRules.Transport,
+    name: 'Transport',
+    placeholderName: t(
+      TranslationNames.taskTypePriceForm.formPlaceholder.transport,
+    ),
+    textInputProp: {
+      keyboardType: 'numeric',
+      placeholder: t(
+        TranslationNames.taskTypePriceForm.validation.priceRequired,
+      ),
+    },
+  },
+  {
+    control,
+    rules: TaskTypePriceRules.Transport,
+    name: 'Tax',
+    placeholderName: t(TranslationNames.taskTypePriceForm.formPlaceholder.tax),
+    textInputProp: {
+      keyboardType: 'numeric',
+      placeholder: t(TranslationNames.taskTypePriceForm.formPlaceholder.tax),
+    },
+  },
+];
