ca3f6362018e0d631317d9ab90b4f4021177e0ab
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-10 20:36:58 +0200

    fixed calculations

--- components/atoms/AccountingFieldInfo.tsx
+++ components/atoms/AccountingFieldInfo.tsx
@@ -6,12 +6,16 @@ import { OrderAccountingFormI } from '../../types/self/common/types';
 export interface AccountingFieldInfoProps {
   item: TaskResponseBase;
   tax?: number;
-  watch?: (name: keyof OrderAccountingFormI) => number;
+  prices?: {
+    [K in keyof typeof TaskType]: number | undefined;
+  };
+  watch?: (name?: keyof OrderAccountingFormI) => number;
 }
 
 export function AccountingFieldInfo({
   item,
   watch,
+  prices,
   tax,
 }: AccountingFieldInfoProps) {
   const fieldArea = Number.isNaN(Number(item.field.area))
@@ -20,18 +24,29 @@ export function AccountingFieldInfo({
   const taxValue = Number.isNaN(Number(watch ? watch('Tax') : tax))
     ? 0
     : Number(watch ? watch('Tax') : tax);
+
   const priceForTask = Number.isNaN(
     Number(
-      watch ? watch(TaskType[item.type] as keyof OrderAccountingFormI) : tax,
+      watch
+        ? watch(TaskType[item.type] as keyof OrderAccountingFormI)
+        : prices &&
+            prices[TaskType[item.type as unknown as keyof typeof TaskType]],
     ),
   )
     ? 0
     : Number(
-        watch ? watch(TaskType[item.type] as keyof OrderAccountingFormI) : tax,
+        watch
+          ? watch(TaskType[item.type] as keyof OrderAccountingFormI)
+          : prices &&
+              prices[TaskType[item.type as unknown as keyof typeof TaskType]],
       );
+  console.log(priceForTask);
   const priceWithoutTax = fieldArea * priceForTask;
   const priceWithTax =
-    taxValue && priceForTask ? fieldArea * (1 + taxValue) : 0;
+    (taxValue || (watch && watch('Tax'))) && priceWithoutTax
+      ? priceWithoutTax * (1 + taxValue || (watch && watch('Tax')) || 0)
+      : 0;
+
   return (
     <XStack ai="center" jc="space-between" f={1} maxHeight={20}>
       <SizableText className="flex-1 text-left uppercase font-bold text-light-blue dark:text-white">
