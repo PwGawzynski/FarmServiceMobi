123f28d315922de7e5f3afda6b9f5f0f16e4c3d7
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-15 13:18:59 +0200

    created translation file

---
+++ helepers/Translations/AccountingSummaryTranslations.tsx
@@ -0,0 +1,119 @@
+import { t } from 'i18next';
+import { TranslationNames } from '../../locales/TranslationNames';
+
+export const TRANSLATIONS = {
+  clientInvoiceData: t(
+    TranslationNames.screens.orderDriver.accountingSummary.clientInvoiceData,
+  ),
+  useClientsCompanyData: t(
+    TranslationNames.screens.orderDriver.accountingSummary
+      .useClientsCompanyData,
+  ),
+  createCompanyData: t(
+    TranslationNames.screens.orderDriver.accountingSummary.createCompanyData,
+  ),
+  companyName: t(
+    TranslationNames.screens.orderDriver.accountingSummary.companyName,
+  ),
+  companyNIP: t(
+    TranslationNames.screens.orderDriver.accountingSummary.companyNIP,
+  ),
+  county: t(TranslationNames.screens.orderDriver.accountingSummary.county),
+  apartmentNumber: t(
+    TranslationNames.screens.orderDriver.accountingSummary.apartmentNumber,
+  ),
+  voivodeship: t(
+    TranslationNames.screens.orderDriver.accountingSummary.voivodeship,
+  ),
+  name: t(TranslationNames.screens.orderDriver.accountingSummary.name),
+  surname: t(TranslationNames.screens.orderDriver.accountingSummary.surname),
+  phoneNumber: t(
+    TranslationNames.screens.orderDriver.accountingSummary.phoneNumber,
+  ),
+  orderName: t(
+    TranslationNames.screens.orderDriver.accountingSummary.orderName,
+  ),
+  performanceDate: t(
+    TranslationNames.screens.orderDriver.accountingSummary.performanceDate,
+  ),
+  totalArea: t(
+    TranslationNames.screens.orderDriver.accountingSummary.totalArea,
+  ),
+  status: t(TranslationNames.screens.orderDriver.accountingSummary.status),
+  openedAt: t(TranslationNames.screens.orderDriver.accountingSummary.openedAt),
+  createdAt: t(
+    TranslationNames.screens.orderDriver.accountingSummary.createdAt,
+  ),
+  taxValue: t(TranslationNames.screens.orderDriver.accountingSummary.taxValue),
+  totalPrice: t(
+    TranslationNames.screens.orderDriver.accountingSummary.totalPrice,
+  ),
+  totalPriceWithTax: t(
+    TranslationNames.screens.orderDriver.accountingSummary.totalPriceWithTax,
+  ),
+  harvestingTotalArea: t(
+    TranslationNames.screens.orderDriver.accountingSummary.harvestingTotalArea,
+  ),
+  transportTotalArea: t(
+    TranslationNames.screens.orderDriver.accountingSummary.transportTotalArea,
+  ),
+  harvestingTotalPrice: t(
+    TranslationNames.screens.orderDriver.accountingSummary.harvestingTotalPrice,
+  ),
+  transportTotalPrice: t(
+    TranslationNames.screens.orderDriver.accountingSummary.transportTotalPrice,
+  ),
+  companyDataIsEmpty: t(
+    TranslationNames.screens.orderDriver.accountingSummary.companyDataIsEmpty,
+  ),
+  clientNoCompanyDesc: t(
+    TranslationNames.screens.orderDriver.accountingSummary.clientNoCompany,
+  ),
+  companyEmail: t(
+    TranslationNames.screens.orderDriver.accountingSummary.companyEmail,
+  ),
+  companyPhoneNumber: t(
+    TranslationNames.screens.orderDriver.accountingSummary.companyPhoneNumber,
+  ),
+  city: t(TranslationNames.screens.orderDriver.accountingSummary.city),
+  street: t(TranslationNames.screens.orderDriver.accountingSummary.street),
+  postalCode: t(
+    TranslationNames.screens.orderDriver.accountingSummary.postalCode,
+  ),
+  houseNumber: t(
+    TranslationNames.screens.orderDriver.accountingSummary.houseNumber,
+  ),
+  fillCompanyData: t(
+    TranslationNames.screens.orderDriver.accountingSummary.fillCompanyData,
+  ),
+  usingClientPersonalDataHintHeader: t(
+    TranslationNames.screens.orderDriver.accountingSummary
+      .usingClientPersonalDataHintHeader,
+  ),
+  usingClientPersonalDataHintDesc: t(
+    TranslationNames.screens.orderDriver.accountingSummary
+      .usingClientPersonalDataHintDesc,
+  ),
+  noData: t(TranslationNames.screens.orderDriver.accountingSummary.noData),
+
+  alertTitle: t(
+    TranslationNames.screens.orderDriver.accountingSummary.alertTitle,
+  ),
+  alertDescription: t(
+    TranslationNames.screens.orderDriver.accountingSummary.alertDescription,
+  ),
+  clientCardName: t(
+    TranslationNames.screens.orderDriver.accountingSummary.clientCardName,
+  ),
+  clientCardTopRightButtonName: t(
+    TranslationNames.screens.orderDriver.accountingSummary
+      .clientCardTopRightButtonName,
+  ),
+  orderCardName: t(
+    TranslationNames.screens.orderDriver.accountingSummary.orderCardName,
+  ),
+  generateInvoiceButton: t(
+    TranslationNames.screens.orderDriver.accountingSummary
+      .generateInvoiceButton,
+  ),
+};
