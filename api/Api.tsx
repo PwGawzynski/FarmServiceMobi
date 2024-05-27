fcae71560a1c681f80a725bbb849908fcc41bd64
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-27 22:39:59 +0200

    changed location due to circular ref

--- api/Api.tsx
+++ api/Api.tsx
@@ -20,7 +20,6 @@ import {
 import { UserResponseBase } from '../FarmServiceApiTypes/User/Responses';
 import { CreateUserReqI } from '../FarmServiceApiTypes/User/Requests';
 import { IsDelayed } from '../helepers/Api/decorators/IsDelayed';
-import { methodDecorator } from '../helepers/Api/AutoRefreshToken';
 
 /* ---------------------------------------API_CLASS--------------------------------------- */
 
@@ -450,6 +449,46 @@ export class ApiSelf {
     });
     return eventSource;
   }
+}
+
+// ----------------------------------------DECORATOR---------------------------------------------//
+const API_EXCLUDED_FROM_AUTO_REFRESH = [
+  'loginUser',
+  'init',
+  'googleLogin',
+  'registerNewUser',
+  'logout',
+  'isMailFree',
+  'saveTokensToSecureStoreFromResPayload',
+];
+
+function methodDecorator(
+  // eslint-disable-next-line no-param-reassign,@typescript-eslint/no-explicit-any
+  target: any,
+  key: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalMethod = descriptor.value;
+
+  // eslint-disable-next-line no-param-reassign,@typescript-eslint/no-explicit-any,func-names
+  descriptor.value = async function (...args: any[]) {
+    //
+    if (API_EXCLUDED_FROM_AUTO_REFRESH.includes(key))
+      return originalMethod.apply(this, args);
+    const tokenRestorationStart = Date.now();
+    const tokens = await ApiSelf.session();
+    const tokenRestorationEnd = Date.now();
+    console.log(
+      ` \n \n During execution ${key}, determined tokens as ${
+        tokens ? 'actual' : 'stale(system executed Api.restoreTokens)'
+      }, operation has been processed in  ~ ${
+        tokenRestorationEnd - tokenRestorationStart
+      } ms \n \n `,
+    );
+    return originalMethod.apply(this, args);
+  };
+
+  return descriptor;
 }
 
 const handlers = {
