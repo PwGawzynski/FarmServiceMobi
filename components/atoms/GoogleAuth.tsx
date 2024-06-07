eb2f28f2d2be900c0c0282a5efc0421d1e2cf09e
pwgawzynski <pgawzynski.backup@gmail.com>
2024-06-07 07:40:45 +0200

    added default error handler

--- components/atoms/GoogleAuth.tsx
+++ components/atoms/GoogleAuth.tsx
@@ -1,6 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
 import { useEffect } from 'react';
-import { Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { t } from 'i18next';
 import { loginByGoogle } from '../../api/services/User';
@@ -11,18 +10,17 @@ import { TranslationNames } from '../../locales/TranslationNames';
 import { AuthDriverProps } from '../../types/self/navigation/Owner/props/AuthDriverProps';
 import GoogleIco from '../../assets/google.svg';
 import { ButtonTamagui } from './ButtonTamagui';
+import { DefaultQueryErrorHandler } from '../../helepers/Api/DefaultQueryErrorHandler';
 
 export function GoogleAuth({
   navigation,
 }: AuthDriverProps<'chooseLoginType' | 'chooseRegisterType'>) {
-  const { mutate, data, error } = useMutation({
+  const { mutate, data } = useMutation({
     mutationKey: ['loginByGoogle'],
     mutationFn: loginByGoogle,
+    onError: DefaultQueryErrorHandler,
   });
   const signIn = () => googleSignIn(mutate);
-  useEffect(() => {
-    if (error) Alert.alert('Error', error.message);
-  }, [error]);
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     roleBasedRedirection(data, navigation, dispatch);
