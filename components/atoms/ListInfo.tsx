e99ac388b01f66e7e4dc5eb87080d27444f8aff9
pwgawzynski <pgawzynski.backup@gmail.com>
2024-06-07 07:41:34 +0200

    changed design

--- components/atoms/ListInfo.tsx
+++ components/atoms/ListInfo.tsx
@@ -1,37 +1,25 @@
 import { Dimensions, View } from 'react-native';
-import { SizableText, useTheme, YStack } from 'tamagui';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { t } from 'i18next';
 import { TranslationNames } from '../../locales/TranslationNames';
-import Empty from '../../assets/empty.svg';
+import { HintCard } from './HintCard';
 
 export type ListEmptyProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  Ico?: any;
   height?: number;
   text?: string;
-  children?: React.ReactElement | React.ReactElement[] | boolean;
-  color?: string;
   beFlex?: boolean;
   isDelayed?: boolean;
 };
 
 const TRANSLATIONS = {
   unknownError: t(TranslationNames.components.listInfo.unknownError),
+  emptyListHeader: t(TranslationNames.components.listInfo.emptyListHeader),
 };
 const LIST_EMPTY_DELAY = 50;
 
-export function ListInfo({
-  Ico,
-  height,
-  text,
-  children,
-  color: _color,
-  beFlex,
-  isDelayed,
-}: ListEmptyProps) {
+export function ListInfo({ height, text, beFlex, isDelayed }: ListEmptyProps) {
   const EMPTY_HEIGHT = height ?? Dimensions.get('window').height * 0.7;
-  const { color } = useTheme();
   const [canShow, setCanShow] = useState(false);
   useEffect(() => {
     if (isDelayed) {
@@ -49,31 +37,12 @@ export function ListInfo({
         height: beFlex ? undefined : EMPTY_HEIGHT,
         flex: beFlex ? 1 : undefined,
       }}
-      className="flex-1 items-center justify-center"
+      className="flex-1"
     >
-      <YStack ai="center" jc="center" f={1}>
-        {Ico ? (
-          <Ico
-            width={EMPTY_HEIGHT / 10}
-            height={EMPTY_HEIGHT / 10}
-            color={_color ?? color?.val}
-          />
-        ) : (
-          <Empty
-            width={EMPTY_HEIGHT / 10}
-            height={EMPTY_HEIGHT / 10}
-            color={_color ?? color?.val}
-          />
-        )}
-        <SizableText
-          color={_color ?? color?.val}
-          className="mt-8  text-center"
-          fontSize="$5"
-        >
-          {text ?? TRANSLATIONS.unknownError}
-        </SizableText>
-        {children}
-      </YStack>
+      <HintCard
+        header={TRANSLATIONS.emptyListHeader}
+        text={text ?? TRANSLATIONS.unknownError}
+      />
     </View>
   );
 }
